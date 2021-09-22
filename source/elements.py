f8db58e1983510b4e3c5b0d680ea74aa229888d9
Adrien P <placais@lpsc.in2p3.fr>
2021-09-22 16:03:33 +0200

    Added the type of the parameters imported in QUAD and DRIFT.

--- source/elements.py
+++ source/elements.py
@@ -57,13 +57,13 @@ class Drift(Element):
                 'Wrong number of arguments for DRIFT element at position '
                 + str(self.element_pos))
 
-        self.L = line[1]
-        self.R = line[2]
+        self.L = float(line[1])
+        self.R = float(line[2])
 
         try:
-            self.R_y = line[3]
-            self.R_x_shift = line[4]
-            self.R_y_shift = line[5]
+            self.R_y = float(line[3])
+            self.R_x_shift = float(line[4])
+            self.R_y_shift = float(line[5])
         except IndexError:
             pass
 
@@ -82,17 +82,17 @@ class Quad(Element):
                 'Wrong number of arguments for QUAD element at position '
                 + str(self.element_pos))
 
-        self.L = line[1]
-        self.G = line[2]
-        self.R = line[3]
+        self.L = float(line[1])
+        self.G = float(line[2])
+        self.R = float(line[3])
 
         try:
-            self.Theta = line[4]
-            self.G3_over_u3 = line[5]
-            self.G4_over_u4 = line[6]
-            self.G5_over_u5 = line[7]
-            self.G6_over_u6 = line[8]
-            self.GFR = line[9]
+            self.Theta = float(line[4])
+            self.G3_over_u3 = float(line[5])
+            self.G4_over_u4 = float(line[6])
+            self.G5_over_u5 = float(line[7])
+            self.G6_over_u6 = float(line[8])
+            self.GFR = float(line[9])
         except IndexError:
             pass
 
