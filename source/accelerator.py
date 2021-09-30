Implemented cavities transfer matrices. Calculation of first cavity is not correct.

source/accelerator.py
@@ -9,13 +9,7 @@ import numpy as np
 import elements as elem
 import helper
 import transfer_matrices
-
-
-# =============================================================================
-# Physical constants
-# =============================================================================
-c = 2.99792458e8
-m_proton = 938.272  # In MeV/c**2
+from constants import c, m_MeV
 
 
 class Accelerator():
@@ -49,10 +43,11 @@ class Accelerator():
 
         # Array containing gamma at each in/out element
         self.gamma = np.full((self.n_elements + 1), np.NaN)
-        self.gamma[0] = 1. + E_MeV / m_proton
+        self.gamma[0] = 1. + E_MeV / m_MeV
 
         # Common to every element
         self.elements_nature = np.full((self.n_elements), np.NaN, dtype=object)
+        self.elements_resume = np.full((self.n_elements), np.NaN, dtype=object)
         self.z_transfer_func = np.full((self.n_elements), np.NaN, dtype=object)
 
         # Elements length, apertures
@@ -86,6 +81,11 @@ class Accelerator():
         self.K_a = np.full((self.n_elements), np.NaN)
         self.FileName = np.full((self.n_elements), np.NaN, dtype=object)
         self.P = np.full((self.n_elements), np.NaN)
+        # Import
+        self.nz = np.full((self.n_elements), np.NaN, dtype=int)
+        self.zmax = np.full((self.n_elements), np.NaN)
+        self.Norm = np.full((self.n_elements), np.NaN)
+        self.Fz_array = np.full((self.n_elements), np.NaN, dtype=object)
 
         # Specific to SPACE_CHARGE_COMP
         self.k = np.full((self.n_elements), np.NaN)
@@ -186,7 +186,7 @@ class Accelerator():
             idx_max = self.n_elements
 
         for i in range(idx_min, idx_max):
-            print(self.elements_nature[i])
+            print(self.elements_resume[i])
 
     def compute_transfer_matrix_and_gamma(self, idx_min=0, idx_max=0):
         """
@@ -206,15 +206,31 @@ class Accelerator():
         """
         # TODO: handle acceleration of particle
         # TODO: precompute gamma at each element entrance. Required in order
-        # to compute transfer natrics of a subset of elements.
+        # to compute transfer matrice of a subset of elements.
         if(idx_max == 0):
             idx_max = self.n_elements
 
         R_zz_tot = np.eye(2, 2)
 
         for i in range(idx_min, idx_max):
-            R_zz_next = self.z_transfer_func[i](self.L_m[i], self.gamma[i])
+            if(self.elements_nature[i] == 'FIELD_MAP'):
+                # FIXME harmonize with other elements
+                # TODO Check this Ncell truc.
+                R_zz_next, self.E_MeV[i+1:] = transfer_matrices.z_field_map_electric_field(
+                        self.E_MeV[i], self.f_MHz[i], self.Fz_array[i],
+                        self.k_e[i], self.theta_i[i], 2, self.nz[i],
+                        self.zmax[i])
+                beta = np.sqrt((1. + self.E_MeV[i+1] / m_MeV)**2 - 1.) / (1. +
+                                                                          self.E_MeV[i+1] / m_MeV)
+                # TODO Functions MeV to beta, beta to MeV, MeV to gamma, etc
+                # TODO Even better: a setter to update all these arrays
+                # together.
+                self.gamma[i+1] = 1. / np.sqrt(1. - beta**2)
+
+            else:
+                R_zz_next = self.z_transfer_func[i](self.L_m[i], self.gamma[i])
+                self.gamma[i+1] = self.gamma[i]
+
             R_zz_tot = np.matmul(R_zz_tot, R_zz_next)
-            self.gamma[i+1] = self.gamma[i]  # TODO check
 
         return R_zz_tot

source/cavity_matriX.py
@@ -9,7 +9,6 @@ Created on Wed Sep 29 14:10:33 2021
 import numpy as np
 import matplotlib.pyplot as plt
 from elements import select_and_load_field_map_file
-from essais import electric_field
 from scipy.interpolate import interp1d
 from transfer_matrices import z_drift
 from helper import MeV_to_v, v_to_MeV
@@ -35,7 +34,7 @@ debug_plot = True
 f_Hz = 352.2e6
 omega_0 = 2. * np.pi * f_Hz
 
-N_cell = 500
+N_cell = 2
 
 phi_RG_deg = 142.089    # LINAC.theta_i[36]
 phi_RF = np.deg2rad(phi_RG_deg)
@@ -48,7 +47,7 @@ beta_0 = np.sqrt((1. + E_0_MeV / m_MeV)**2 - 1.) / (1. + E_0_MeV / m_MeV)
 gamma_0 = 1. / np.sqrt(1. - beta_0**2)
 
 # Coef on the field
-k = 1.68927 * 1.000092819734090     # LINAC.k_e[36] * ?
+k = 1.68927  # * 1.000092819734090     # LINAC.k_e[36] * ?
 # Load electric field
 nz, zmax, Norm, Fz_array = select_and_load_field_map_file(
     '/home/placais/TraceWin/work_field_map/work_field_map.dat',
@@ -57,8 +56,6 @@ Fz_array *= k
 
 z_cavity_array = np.linspace(0., zmax, nz + 1)      # z cav local coordinates
 Fz_func = interp1d(z_cavity_array, Fz_array)    # interpolate field
-
-cavity_field = electric_field(Norm, Fz_func, omega_0, phi_RF)
 
 dz = z_cavity_array[1] - z_cavity_array[0]
 dE_dz_array = np.diff(Fz_array) / dz
@@ -73,7 +70,7 @@ dE_dz_func = interp1d((z_cavity_array[:-1] + z_cavity_array[1:]) * 0.5,
 # Simulation parameters
 # =============================================================================
 # Initial step size calculated with betalambda
-factor = 2.
+factor = 100.
 lambda_RF = c / f_Hz
 # Spatial step
 step = beta_0 * lambda_RF / (2. * N_cell * factor)

source/elements.py
@@ -9,14 +9,9 @@ import os
 import numpy as np
 import helper
 import transfer_matrices
-from scipy.interpolate import interp1d
-from scipy.integrate import quad
 
 
-c = 2.99792458e8
-
-
-def add_drift(Accelerator, line, i):
+def add_drift(accelerator, line, i):
     """Add a linear drift to the Accelerator object."""
     n_attributes = len(line) - 1
 
@@ -28,22 +23,23 @@ def add_drift(Accelerator, line, i):
             'Wrong number of arguments for DRIFT element at position '
             + str(i))
 
-    Accelerator.elements_nature[i] = str(i) + ' \t' + '\t'.join(line)
-    Accelerator.L_mm[i] = float(line[1])
-    Accelerator.L_m[i] = Accelerator.L_mm[i] * 1e-3
-    Accelerator.R[i] = float(line[2])
+    accelerator.elements_resume[i] = str(i) + ' \t' + '\t'.join(line)
+    accelerator.elements_nature[i] = 'DRIFT'
+    accelerator.L_mm[i] = float(line[1])
+    accelerator.L_m[i] = accelerator.L_mm[i] * 1e-3
+    accelerator.R[i] = float(line[2])
 
     try:
-        Accelerator.R_y[i] = float(line[3])
-        Accelerator.R_x_shift[i] = float(line[4])
-        Accelerator.R_y_shift[i] = float(line[5])
+        accelerator.R_y[i] = float(line[3])
+        accelerator.R_x_shift[i] = float(line[4])
+        accelerator.R_y_shift[i] = float(line[5])
     except IndexError:
         pass
 
-    Accelerator.z_transfer_func[i] = transfer_matrices.z_drift
+    accelerator.z_transfer_func[i] = transfer_matrices.z_drift
 
 
-def add_quad(Accelerator, line, i):
+def add_quad(accelerator, line, i):
     """Add a quadrupole to the Accelerator object."""
     n_attributes = len(line) - 1
 
@@ -54,27 +50,28 @@ def add_quad(Accelerator, line, i):
             'Wrong number of arguments for QUAD element at position '
             + str(i))
 
-    Accelerator.elements_nature[i] = str(i) + ' \t' + '\t'.join(line)
+    accelerator.elements_resume[i] = str(i) + ' \t' + '\t'.join(line)
+    accelerator.elements_nature[i] = 'QUAD'
 
-    Accelerator.L_mm[i] = float(line[1])
-    Accelerator.L_m[i] = Accelerator.L_mm[i] * 1e-3
-    Accelerator.G[i] = float(line[2])
-    Accelerator.R[i] = float(line[3])
+    accelerator.L_mm[i] = float(line[1])
+    accelerator.L_m[i] = accelerator.L_mm[i] * 1e-3
+    accelerator.G[i] = float(line[2])
+    accelerator.R[i] = float(line[3])
 
     try:
-        Accelerator.Theta[i] = float(line[4])
-        Accelerator.G3_over_u3[i] = float(line[5])
-        Accelerator.G4_over_u4[i] = float(line[6])
-        Accelerator.G5_over_u5[i] = float(line[7])
-        Accelerator.G6_over_u6[i] = float(line[8])
-        Accelerator.GFR[i] = float(line[9])
+        accelerator.Theta[i] = float(line[4])
+        accelerator.G3_over_u3[i] = float(line[5])
+        accelerator.G4_over_u4[i] = float(line[6])
+        accelerator.G5_over_u5[i] = float(line[7])
+        accelerator.G6_over_u6[i] = float(line[8])
+        accelerator.GFR[i] = float(line[9])
     except IndexError:
         pass
 
-    Accelerator.z_transfer_func[i] = transfer_matrices.z_drift
+    accelerator.z_transfer_func[i] = transfer_matrices.z_drift
 
 
-def add_solenoid(Accelerator, line, i):
+def add_solenoid(accelerator, line, i):
     """Add a solenoid to the Accelerator object."""
     n_attributes = len(line) - 1
 
@@ -84,17 +81,18 @@ def add_solenoid(Accelerator, line, i):
             'Wrong number of arguments for SOLENOID element at position '
             + str(i))
 
-    Accelerator.elements_nature[i] = str(i) + ' \t' + '\t'.join(line)
+    accelerator.elements_resume[i] = str(i) + ' \t' + '\t'.join(line)
+    accelerator.elements_nature[i] = 'SOLENOID'
 
-    Accelerator.L_mm[i] = float(line[1])
-    Accelerator.L_m[i] = Accelerator.L_mm[i] * 1e-3
-    Accelerator.B[i] = float(line[2])
-    Accelerator.R[i] = float(line[3])
+    accelerator.L_mm[i] = float(line[1])
+    accelerator.L_m[i] = accelerator.L_mm[i] * 1e-3
+    accelerator.B[i] = float(line[2])
+    accelerator.R[i] = float(line[3])
 
-    Accelerator.z_transfer_func[i] = transfer_matrices.z_drift
+    accelerator.z_transfer_func[i] = transfer_matrices.z_drift
 
 
-def add_field_map(Accelerator, line, i, TraceWin_dat_filename, f_MHz):
+def add_field_map(accelerator, line, i, TraceWin_dat_filename, f_MHz):
     """
     Add a field map to the Accelerator object.
 
@@ -125,41 +123,46 @@ def add_field_map(Accelerator, line, i, TraceWin_dat_filename, f_MHz):
     n_attributes = len(line) - 1
 
     # FIXME
-    Accelerator.TraceWin_dat_filename = TraceWin_dat_filename
+    accelerator.TraceWin_dat_filename = TraceWin_dat_filename
 
     if((n_attributes < 9) or (n_attributes > 10)):
         raise IOError(
             'Wrong number of arguments for FIELD_MAP element at position '
             + str(i))
 
-    Accelerator.elements_nature[i] = str(i) + ' \t' + '\t'.join(line)
+    accelerator.elements_resume[i] = str(i) + ' \t' + '\t'.join(line)
+    accelerator.elements_nature[i] = 'FIELD_MAP'
 
-    Accelerator.geom[i] = int(line[1])
-    Accelerator.L_mm[i] = float(line[2])
-    Accelerator.L_m[i] = Accelerator.L_mm[i] * 1e-3
-    Accelerator.theta_i[i] = float(line[3])
-    Accelerator.R[i] = float(line[4])
-    Accelerator.k_b[i] = float(line[5])
-    Accelerator.k_e[i] = float(line[6])
-    Accelerator.K_i[i] = float(line[7])
-    Accelerator.K_a[i] = int(line[8])     # FIXME according to doc, may also be float
-    Accelerator.FileName[i] = str(line[9])
+    accelerator.geom[i] = int(line[1])
+    accelerator.L_mm[i] = float(line[2])
+    accelerator.L_m[i] = accelerator.L_mm[i] * 1e-3
+    accelerator.theta_i[i] = float(line[3])
+    accelerator.R[i] = float(line[4])
+    accelerator.k_b[i] = float(line[5])
+    accelerator.k_e[i] = float(line[6])
+    accelerator.K_i[i] = float(line[7])
+    accelerator.K_a[i] = int(line[8])     # FIXME according to doc, may also be float
+    accelerator.FileName[i] = str(line[9])
 
     try:
-        Accelerator.P[i] = int(line[10])
+        accelerator.P[i] = int(line[10])
     except IndexError:
         pass
 
-    Accelerator.z_transfer_func[i] = transfer_matrices.dummy
+    accelerator.z_transfer_func[i] = transfer_matrices.dummy
 
     nz, zmax, Norm, Fz_array = select_and_load_field_map_file(
-        Accelerator.TraceWin_dat_filename,
-        Accelerator.geom[i],
-        Accelerator.K_a[i],
-        Accelerator.FileName[i])
+        accelerator.TraceWin_dat_filename,
+        accelerator.geom[i],
+        accelerator.K_a[i],
+        accelerator.FileName[i])
 
-    # Calculate synchronous phase
-    omega_0 = 2. * np.pi * Accelerator.f_MHz[i]     # Pulsation
+    accelerator.nz[i] = nz
+    accelerator.zmax[i] = zmax
+    accelerator.Norm[i] = Norm
+    accelerator.Fz_array[i] = Fz_array
+    
+    return 
 
 
 def select_and_load_field_map_file(TraceWin_dat_filename, geom, K_a, FileName):
@@ -190,13 +193,13 @@ def select_and_load_field_map_file(TraceWin_dat_filename, geom, K_a, FileName):
     if(os.path.exists(FileName)):
         path = FileName
         if(debug_verbose):
-            #  Accelerator.show_element_info()  # FIXME
+            #  accelerator.show_element_info()  # FIXME
             print("Loading field map with relative filepath...")
 
     elif(os.path.exists(absolute_path)):
         path = absolute_path
         if(debug_verbose):
-            #  Accelerator.show_element_info()  # FIXME
+            #  accelerator.show_element_info()  # FIXME
             print("Loading field map with absolute filepath...")
 
     else:

source/transfer_matrices.py
@@ -10,6 +10,8 @@ exactly as in TraceWin, i.e. first line is z (m) and second line is dp/p.
 """
 
 import numpy as np
+from constants import c, q_adim, m_MeV
+from scipy.interpolate import interp1d
 
 
 # =============================================================================
@@ -45,7 +47,159 @@ def z_drift(Delta_s, gamma):
     return R_zz
 
 
-def not_an_element(Delta_s, gamma):
-    """Return identity matrix."""
+def z_field_map_electric_field(E_0_MeV, f_MHz, Fz_array, k_e, theta_i,
+                               N_cell, nz, zmax):
+    """
+    Compute the transfer matrix of an accelerating cavity.
+
+    In the process, it also computes the variation of energy, the synchronous
+    phase, the accelerating field.
+
+    Parameters
+    ----------
+    E_0_MeV: float
+        Energy at the cavity's entrance in MeV.
+    f_MHz: float
+        Frequency of the cavity in MHz.
+    Fz_array: numpy.array
+        Array of electric field values in V/m.
+    k_e: float
+        Electric field multiplication factor.
+    theta_i: float
+        Phase of particles at the cavity's entrance in deg.
+    N_cell: int
+        Number of cells in the cavity.
+    nz: int
+        Number of points (minus one) in the Fz_array.
+    zmax: float
+        Relative position of the cavity's exit in mm.
+
+    Returns
+    -------
+    R_zz: np.array
+        Transfer matrix of the cavity.
+    E_MeV: float
+        Energy of the particle beam when it goes out of the cavity.
+    ----------
+    """
+    # Set useful parameters
+    omega_0 = 2e6 * np.pi * f_MHz
+    beta_0 = np.sqrt((1. + E_0_MeV / m_MeV)**2 - 1.) / (1. + E_0_MeV / m_MeV)
+    gamma_0 = 1. / np.sqrt(1. - beta_0**2)
+    phi_RF = np.deg2rad(theta_i)
+
+    # Local coordinates of the cavity:
+    z_cavity_array = np.linspace(0., zmax, nz + 1)
+    dz = z_cavity_array[1] - z_cavity_array[0]
+
+    Ez_func = interp1d(z_cavity_array, k_e * Fz_array)
+    dE_dz_array = np.diff(Fz_array) / dz
+
+    # Interpolate dE/dz on the middle of every cell
+    dE_dz_func = interp1d((z_cavity_array[:-1] + z_cavity_array[1:]) * 0.5,
+                          dE_dz_array,
+                          fill_value='extrapolate')
+    # We use fill_value=extrapolate to define the function on [0, .5*dz] and
+    # [zmax-.5*dz, zmax].
+
+    # =========================================================================
+    # Simulation parameters
+    # =========================================================================
+    # Initial step size calculated with betalambda
+    factor = 100.
+    lambda_RF = 1e-6 * c / f_MHz
+    # Spatial step
+    step = beta_0 * lambda_RF / (2. * N_cell * factor)
+
+    # =========================================================================
+    # Compute energy gain and synchronous phase
+    # =========================================================================
+    # Step size
+    idx_max = int(np.floor(zmax / step))
+
+    # Init synchronous particle
+    E_out_MeV = E_0_MeV
+    gamma_out = gamma_0
+    z = .5 * step   # Middle of first spatial step
+
+    E_r = 0.
+    E_i = 0.
+
+    t = step / (2. * beta_0 * c)
+
+    # TODO: check if script is faster with numpy arrays
+    z_pos_array = [0.]
+    energy_array = [E_0_MeV]
+    beta_array = [beta_0]
+    gamma_array = [gamma_0]
+
     R_zz = np.eye(2, 2)
-    return R_zz
+
+    # Loop over the spatial steps
+    for i in range(idx_max):
+        E_in_MeV = E_out_MeV
+        gamma_in = gamma_out
+
+        # Compute energy gain
+        E_interp = Ez_func(z)[()]
+        acceleration = q_adim * E_interp * np.cos(phi_RF)
+        E_r += acceleration
+        E_i += q_adim * E_interp * np.sin(phi_RF)
+
+        # Energy and gamma at the exit of current cell
+        E_out_MeV = acceleration * step + E_in_MeV
+        gamma_out = gamma_in + acceleration * step / m_MeV
+
+        # Synchronous gamma
+        gamma_s = (gamma_out + gamma_in) * .5
+        beta_s = np.sqrt(1. - 1. / gamma_s**2)
+        beta_out = np.sqrt(1. - 1. / gamma_out**2)
+
+        # Synchronous phase
+        phi_s = np.arctan(E_i / E_r)
+        phi_s_deg = np.rad2deg(phi_s)
+
+        # Compute transfer matrix
+        K_0 = q_adim * np.cos(phi_RF) * step / (gamma_s * beta_s**2 * m_MeV)
+        K_1 = dE_dz_func(z) * K_0
+        K_2 = 1. - (2. - beta_s**2) * Ez_func(z) * K_0
+
+        M_in = z_drift(.5 * step, gamma_in)
+        M_mid = np.array(([1., 0.],
+                          [K_1, K_2]))
+        M_out = z_drift(.5 * step, gamma_out)
+
+        # Compute M_in * M_mid * M_out * M_t
+        R_zz = np.matmul(np.matmul(np.matmul(M_in, M_mid), M_out), R_zz)
+
+        # Next step
+        z += step
+        t += step / (beta_s * c)
+        phi_RF += step * omega_0 / (beta_s * c)
+
+        # Save data
+        z_pos_array.append(z)
+        energy_array.append(E_out_MeV)
+        beta_array.append(beta_out)   # TODO: check this!
+        gamma_array.append(gamma_out)     # TODO: check this!
+
+    # =========================================================================
+    # End of loop
+    # =========================================================================
+    z_pos_array = np.array(z_pos_array)
+    energy_array = np.array(energy_array)
+    beta_array = np.array(beta_array)
+    gamma_array = np.array(gamma_array)
+
+    E_MeV = energy_array[-1]
+
+    # TODO: save and/or output V_cav and phi_s
+    # V_cav_MV = np.abs((E_MeV - E_0_MeV) / np.cos(phi_s))
+    # phi_s_deg = phi_s_deg
+
+    return R_zz, E_MeV
+
+    def not_an_element(Delta_s, gamma):
+        """Return identity matrix."""
+        R_zz = np.eye(2, 2)
+        return R_zz