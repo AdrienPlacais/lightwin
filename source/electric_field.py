Fixed relative phase calculation.

source/electric_field.py
@@ -26,7 +26,7 @@ class RfField():
         phi = omega_0_bunch * t
     """
 
-    def __init__(self, frequency_mhz, norm=np.NaN, relative_phase_flag=0,
+    def __init__(self, frequency_mhz, norm=np.NaN, absolute_phase_flag=0,
                  phi_0=None):
         self.f_mhz_rf = frequency_mhz
         self.omega0_rf = 2e6 * np.pi * frequency_mhz
@@ -40,8 +40,8 @@ class RfField():
         self.e_spat = lambda x: 0.
 
         self.norm = norm
-        self.relative_phase_flag = relative_phase_flag
-        self.set_phi_0(phi_0, absolute=not bool(relative_phase_flag))
+        self.absolute_phase_flag = bool(absolute_phase_flag)
+        self.set_phi_0(phi_0, absolute=self.absolute_phase_flag)
 
         self.n_cell = 2
         self.f_e = 0.
@@ -174,6 +174,7 @@ class RfField():
         phi_rf_abs : float
             Absolute phase of the particle at the entrance of the cavity.
         """
+        print('rel to abs')
         phi_0_abs = self.phi_0['rel'] - phi_rf_abs
         phi_0_abs = np.mod(phi_0_abs, 2. * np.pi)
         self.phi_0['abs'] = phi_0_abs
@@ -187,6 +188,7 @@ class RfField():
         phi_rf_abs : float
             Absolute phase of the particle at the entrance of the cavity.
         """
+        print('abs to rel')
         phi_0_rel = self.phi_0['abs'] + phi_rf_abs
         phi_0_rel = np.mod(phi_0_rel, 2. * np.pi)
         self.phi_0['rel'] = phi_0_rel

source/elements.py
@@ -150,14 +150,14 @@ class FieldMap(_Element):
         self.field_map_file_name = str(elem[9])         # FileName
 
         try:
-            relative_phase_flag = int(elem[10])    # P
+            absolute_phase_flag = int(elem[10])    # P
         except IndexError:
             # Relative by default
             elem.append('0')
-            relative_phase_flag = int(elem[10])
+            absolute_phase_flag = int(elem[10])
 
         self.acc_field = RfField(352.2, norm=float(elem[6]),
-                                 relative_phase_flag=relative_phase_flag,
+                                 absolute_phase_flag=absolute_phase_flag,
                                  phi_0=np.deg2rad(float(elem[3])))
         # FIXME frequency import
         self.status = {

source/particle.py
@@ -255,6 +255,7 @@ class Particle():
         """
         self.phi['rel'] = 0.
         self.z['rel'] = 0.
+        self.f_e = 0.
 
         self._set_omega_rf(acc_field.omega0_rf)
         self.phi['abs_rf'] = self.phi['abs'] * self.frac_omega['bunch_to_rf']
@@ -263,8 +264,7 @@ class Particle():
             # Ref linac: we compute every missing phi_0
             if self.info['reference']:
                 acc_field.convert_phi_0(self.phi['abs_rf'],
-                                        absolute=not bool(
-                                            acc_field.relative_phase_flag))
+                                        absolute=acc_field.absolute_phase_flag)
             else:
                 # Fixed linac: we compute the missing phi_0 in the compensating
                 # cavities.
@@ -282,11 +282,15 @@ class Particle():
                             acc_field.phi_0['abs'] = None
                 # Non-compensating cavity: should remain unchanged
                 else:
-                    print('enter_cavity: should pompe from ref_linac.')
+                    acc_field.convert_phi_0(phi_rf_abs=self.phi['abs_rf'],
+                                            absolute=self.info['abs_phases'])
 
         else:
             print('Warning enter_cavity! Not sure what will happen with a',
                   'non synchronous particle.')
+
+        print(self.info, flag_cav_comp, np.rad2deg(acc_field.phi_0['rel']),
+              np.rad2deg(acc_field.phi_0['abs']))
 
     def exit_cavity(self, index):
         """Reset frac_omega."""