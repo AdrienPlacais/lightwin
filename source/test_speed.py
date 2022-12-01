586857213fa27342892bcdef24dae35e41905ac4
Adrien P <placais@lpsc.in2p3.fr>
2022-12-01 11:03:34 +0100

    Updated test_speed.

--- source/test_speed.py
+++ source/test_speed.py
@@ -18,13 +18,13 @@ import numpy as np
 import constants
 import accelerator as acc
 import fault_scenario as mod_fs
-from main import WHAT_TO_FIT
 
 # TEST = 'simple'
 TEST = 'compensation'
 
 FILEPATH = os.path.abspath(
-    "../data/faultcomp22/working/MYRRHA_Transi-100MeV.dat"
+    "../data/JAEA/JAEA_ADS_026.dat"
+    # "../data/faultcomp22/working/MYRRHA_Transi-100MeV.dat"
 )
 
 if TEST == 'simple':
@@ -45,11 +45,18 @@ if TEST == 'simple':
 
 elif TEST == 'compensation':
     ref_linac = acc.Accelerator(FILEPATH, 'Working')
-    ref_linac.compute_transfer_matrices()
+    results = ref_linac.elts.compute_transfer_matrices()
+    ref_linac.save_results(results, ref_linac.elts)
 
-    failed_cav = [35]
+    failed_cav = [25]
+    wtf_lsq = {'opti method': 'least_squares',
+               'strategy': 'k out of n',
+               'k': 6, 'l': 2, 'manual list': [],
+               'objective': ['w_kin', 'phi_abs_array', 'mismatch factor'],
+               'position': 'end_mod', 'phi_s fit': True}
     broken_linac = acc.Accelerator(FILEPATH, "Broken")
     fail = mod_fs.FaultScenario(ref_linac, broken_linac, failed_cav,
-                                wtf=WHAT_TO_FIT)
+                                wtf=wtf_lsq)
     fail.fix_all()
-    broken_linac.compute_transfer_matrices()
+    results = broken_linac.elts.compute_transfer_matrices()
+    broken_linac.save_results(results, broken_linac.elts)
