360bd91780fccd6acc718646d1b0217cfe4983bb
Adrien P <placais@lpsc.in2p3.fr>
2023-04-28 12:57:40 +0200

    Better style formatting. Fixed an incorrect 'return True'

--- source/settings_manager.py
+++ source/settings_manager.py
@@ -9,27 +9,27 @@ Handle simulation parameters. In particular:
     - which cavities are broken?
     - how should they be fixed?
 """
-
+import logging
 import json
 
-from util.helper import printc
 
 class SettingsManager():
+    """Surpisingly, a manager for your settings."""
+
     def __init__(self, settings_path: str) -> None:
         """Reads a settings file."""
         # settings = json.load(open(settings_path))
 
-    def generate_wtf(self):
+    def generate_wtf(self) -> list | dict:
         """Generate a proper wtf (list of) dict."""
         wtf = None
         return wtf
 
-
-    def test_wtf(self, l_wtf):
+    def test_wtf(self, l_wtf: list | dict) -> None:
         """Test the 'what_to_fit' dictionaries."""
         if isinstance(l_wtf, dict):
-            printc("settings_manager.test_wtf info:", "a single wtf dict was",
-                   "provided and will be used for all the faults.")
+            logging.info("A single wtf dict was provided and will be used "
+                         + "for all the faults.")
             l_wtf = [l_wtf]
 
         for wtf in l_wtf:
@@ -45,10 +45,11 @@ class SettingsManager():
             if not _test_position(wtf):
                 raise IOError("Wrong argument in wtf['position'].")
 
-            assert 'phi_s fit' in wtf.keys() and isinstance(wtf['phi_s fit'], bool)
+            assert 'phi_s fit' in wtf.keys() and isinstance(
+                wtf['phi_s fit'], bool)
 
             if 'scale_objective' in wtf.keys():
-                assert(len(wtf['scale_objective']) == len(wtf['objective']))
+                assert len(wtf['scale_objective']) == len(wtf['objective'])
 
     def generate_list_of_faults(self):
         """Generate a proper (list of) faults."""
@@ -57,26 +58,20 @@ class SettingsManager():
 
 
 # =============================================================================
-# Testing of wtf
+# Testing of wtf strategy
 # =============================================================================
-def _test_strategy(wtf):
+def _test_strategy(wtf: dict) -> bool:
     """Specific test for the key 'strategy' of what_to_fit."""
     if 'strategy' not in wtf.keys():
-        printc("settings_manager._test_strategy error:", "you must",
-               "provide 'strategy' to tell LightWin how compensating",
-               "cavities are chosen.", color='red')
+        logging.error("You must provide 'strategy' to tell LightWin how "
+                      + "compensating cavities are chosen.")
         return False
 
     # You must provide the number of compensating cavities per faulty
-    # cavity. Close broken cavities are automatically gathered and fixed
+    # cavity. Nearby broken cavities are automatically gathered and fixed
     #  together.
     if wtf['strategy'] == 'k out of n':
-        if not ('k' in wtf.keys() and isinstance(wtf['k'], int)):
-            printc("settings_manager._test_strategy error:", "you must",
-                   "provide k, the number of compensating cavities per",
-                   "failed cavity.", color='red')
-            return False
-        return True
+        return _test_strategy_k_out_of_n(wtf)
 
     # You must provide a list of lists of broken cavities, and the
     # corresponding list of lists of compensating cavities. Broken cavities
@@ -92,45 +87,68 @@ def _test_strategy(wtf):
     #   [153, 157]          -> compensate error at 155
     # ]
     if wtf['strategy'] == 'manual':
-        test = 'manual list' in wtf.keys() \
-            and isinstance(wtf['manual list'], list)
-        if not test:
-            printc("settings_manager._test_strategy error:", "you must",
-                   "provide a list of lists of compensating cavities",
-                   "corresponding to each list of failed cavities.",
-                   color='red')
-            return False
-        print('also check failed cavities')
-        return True
+        return _test_strategy_manual(wtf)
 
-     # You must provide the number of compensating lattices per faulty
-     #  cavity. Close broken cavities are gathered and fixed together.
+    # You must provide the number of compensating lattices per faulty
+    # cavity. Close broken cavities are gathered and fixed together.
     if wtf['strategy'] == 'l neighboring lattices':
-        if not ('l' in wtf.keys() and isinstance(wtf['l'], int)):
-            printc("settings_manager._test_strategy error:", "you must",
-                   "provide l, the number of compensating lattices.",
-                   color='red')
-            return False
-        return True
+        return _test_strategy_l_neighboring_lattices(wtf)
 
-    printc("settings_manager._test_strategy error:", "the 'strategy' key",
-           f"did not match any authorized value ({wtf['strategy']}).",
-           color='red')
+    logging.error("The 'strategy' key did not match any authorized value "
+                  + f"({wtf['strategy']}).")
     return False
 
+
+def _test_strategy_k_out_of_n(wtf: dict) -> bool:
+    """Even more specific test for k out of n strategy."""
+    if 'k' not in wtf.keys():
+        logging.error("You must provide k, the number of compensating "
+                      + "cavities per failed cavity.")
+        return False
+
+    if not isinstance(wtf['k'], int):
+        logging.error("k must be an integer.")
+        return False
+
+    return True
+
+
+def _test_strategy_manual(wtf: dict) -> bool:
+    """Even more specific test for manual strategy."""
+    test = 'manual list' in wtf.keys() and isinstance(wtf['manual list'], list)
+    if not test:
+        logging.error("You must provide a list of lists of compensating "
+                      + "cavities corresponding to each list of failed "
+                      + "cavities.")
+        return False
+    logging.info("You must insure that all the elements in manual list are "
+                 + "cavities.")
+    return True
+
+
+def _test_strategy_l_neighboring_lattices(wtf: dict) -> bool:
+    """Even more specific test for l neighboring lattices strategy."""
+    if not ('l' in wtf.keys() and isinstance(wtf['l'], int)):
+        logging.error("You must provide l, the number of compensating "
+                      + "lattices.")
+        return False
+    return True
+
+
+# =============================================================================
+# Testing of wtf objective
+# =============================================================================
 def _test_objective(wtf):
     """Specific test for the key 'objective' of what_to_fit."""
     if 'objective' not in wtf.keys():
-        printc("settings_manager._test_objective error:", "you must",
-               "provide 'objective' to tell LightWin what it should fit.",
-               color='red')
+        logging.error("You must provide 'objective' to tell LightWin what it "
+                      + "should fit.")
         return False
 
     l_obj = wtf['objective']
     if not isinstance(l_obj, list):
-        printc("settings_manager._test_objective error:", "you must",
-               "provide a list of objective, even if there is only one.",
-               color='red')
+        logging.error("You must provide a list of objective, even if there is "
+                      + "only one.")
         return False
 
     implemented = [
@@ -138,48 +156,53 @@ def _test_objective(wtf):
         'eps_zdelta', 'beta_zdelta', 'gamma_zdelta', 'alpha_zdelta',
         'M_11', 'M_12', 'M_22', 'M_21']
 
-    if not all([obj in implemented for obj in l_obj]):
-        printc("settings_manager._test_objective error:", "at least one",
-               "objective was not recognized.", color='red')
-        printc("settings_manager._test_objective info:", "to add your",
-               "own objective, make sure that 1. it can be returned by",
-               "the Accelerator.get() method, 2. it is present in the.",
-               "util.d_output dictionaries and 3. it is in the above",
-               "'implemented' dict.")
+    if not all(obj in implemented for obj in l_obj):
+        logging.error("At least one objective was not recognized.")
+        logging.info("""To add your own objective, make sure that:
+                     1. it can be returned by the Accelerator.get() method;
+                     2. it is present in the util.d_output dictionaries;
+                     3. it is in the above 'implemented' dict.""")
         return False
 
     return True
 
+
+# =============================================================================
+# Testing of wtf optimisation method
+# =============================================================================
 def _test_opti_method(wtf):
     """Test the optimisation method."""
     if 'opti method' not in wtf.keys():
-        printc("settings_manager._test_opti_method error:", "you must",
-               "provide 'opti method' to tell LightWin what optimisation",
-               "algorithm it should use.", color='red')
+        logging.error("You must provide 'opti method' to tell LightWin what "
+                      + "optimisation algorithm it should use.")
         return False
 
     implemented = ['least_squares', 'PSO']
     # TODO: specific testing for each method (look at the kwargs)
     if wtf['opti method'] not in implemented:
-        printc("settings_manager._test_opti_method error:", "algorithm not",
-               "implemented.", color='red')
+        logging.error("Algorithm not implemented.")
         return False
     return True
 
+
+# =============================================================================
+# Testing of wtf position
+# =============================================================================
 def _test_position(wtf):
     """Test where the objectives are evaluated."""
     if 'position' not in wtf.keys():
-        printc("settings_manager._test_opti_method error:", "you must",
-               "provide 'position' to tell LightWin where objectives",
-               "should be evaluated.", color='red')
+        logging.error("You must provide 'position' to tell LightWin where "
+                      + "objectives should be evaluated.")
         return False
 
     implemented = [
-        'end_mod',      # End of last lattice with a compensating cavity
-        '1_mod_after',  # End of one lattice after last lattice with a compensating cavity
-        'both']         # Both lattices
+        # End of last lattice with a compensating cavity
+        'end_mod',
+        # End of one lattice after last lattice with a compensating cavity
+        '1_mod_after',
+        # Both lattices
+        'both']
     if wtf['position'] not in implemented:
-        printc("settings_manager._test_opti_method error:", "position not",
-               "implemented.", color='red')
+        logging.error("Position not implemented.")
         return False
     return True
