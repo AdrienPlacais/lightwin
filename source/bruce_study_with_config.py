f7a2c5a22256fe8b009546588519a4a3a8f6da46
Adrien P <placais@lpsc.in2p3.fr>
2023-05-11 11:15:41 +0200

    No more tw simulation for speed

--- source/bruce_study_with_config.py
+++ source/bruce_study_with_config.py
@@ -33,8 +33,8 @@ if __name__ == '__main__':
     # =========================================================================
     FLAG_FIX = True
     SAVE_FIX = True
-    FLAG_TW = True
-    RECOMPUTE_REFERENCE = True
+    FLAG_TW = False
+    RECOMPUTE_REFERENCE = False
     FLAG_EVALUATE = False
 
     # =========================================================================
