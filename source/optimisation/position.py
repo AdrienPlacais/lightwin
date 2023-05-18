7f3488d354e04bfa1c7013e248e71c1027bafdd6
Adrien P <placais@lpsc.in2p3.fr>
2023-05-18 12:26:40 +0200

    Created a module to handle the position key

---
+++ source/optimisation/position.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 18 12:21:37 2023
+
+@author: placais
+
+Here we define the function related to the 'position' key of the what_to_fit
+dictionary. In particular, it answers the question:
+    Given this set of faults and compensating cavities, what is the portion of
+    the linac that I need to recompute again and again during the fit?
+"""
