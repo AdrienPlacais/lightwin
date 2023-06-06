851db033104608ef26b2b94232c270d3312685b0
Adrien P <placais@lpsc.in2p3.fr>
2023-06-06 11:24:58 +0200

    SPIRAL2 study.

--- source/main.py
+++ source/main.py
@@ -23,18 +23,19 @@ from visualization import plot
 
 
 if __name__ == '__main__':
-    FILEPATH = "../data/JAEA/JAEA_ADS_026.dat"
-    CONFIG_PATH = 'jaea.ini'
+    FILEPATH = "../data/SPIRAL2/my_spiral2_1d.dat"
+    CONFIG_PATH = 'spiral2.ini'
     KEY_SOLVER = 'solver.envelope_longitudinal'
-    KEY_BEAM = 'beam.jaea'
-    KEY_WTF = 'wtf.global'
+    KEY_BEAM = 'beam.My_particle'
+    KEY_WTF = 'wtf.quick_debug'
     KEY_TW = 'tracewin.quick_debug'
 
     # =========================================================================
     # Fault compensation
     # =========================================================================
-    FLAG_FIX = True
-    SAVE_FIX = True
+    FLAG_BREAK = False
+    FLAG_FIX = False
+    SAVE_FIX = False
     FLAG_TW = False
     RECOMPUTE_REFERENCE = False
     FLAG_EVALUATE = False
@@ -80,7 +81,7 @@ if __name__ == '__main__':
     ref_linac = acc.Accelerator(FILEPATH, PROJECT_FOLDER, "Working")
     results = ref_linac.elts.compute_transfer_matrices()
     ref_linac.store_results(results, ref_linac.elts)
-
+    data_tab_from_tw = tw.output_data_in_tw_fashion(ref_linac)
     linacs = [ref_linac]
 
     lw_fit_evals = []
@@ -94,44 +95,45 @@ if __name__ == '__main__':
     if 'manual list' in d_wtf:
         l_manual = d_wtf.pop('manual list')
 
-    for i, failed in enumerate(l_failed):
-        start_time = time.monotonic()
-        lin = acc.Accelerator(FILEPATH, PROJECT_FOLDER, "Broken")
+    if FLAG_BREAK:
+        for i, failed in enumerate(l_failed):
+            start_time = time.monotonic()
+            lin = acc.Accelerator(FILEPATH, PROJECT_FOLDER, "Broken")
 
-        if l_manual is not None:
-            manual = l_manual[i]
-        fault_scenario = FaultScenario(ref_acc=ref_linac,
-                                       fix_acc=lin,
-                                       wtf=d_wtf,
-                                       fault_idx=failed,
-                                       comp_idx=manual)
-        linacs.append(deepcopy(lin))
+            if l_manual is not None:
+                manual = l_manual[i]
+            fault_scenario = FaultScenario(ref_acc=ref_linac,
+                                           fix_acc=lin,
+                                           wtf=d_wtf,
+                                           fault_idx=failed,
+                                           comp_idx=manual)
+            linacs.append(deepcopy(lin))
 
-        if FLAG_FIX:
-            fault_scenario.fix_all()
-            results = lin.elts.compute_transfer_matrices()  # useful?
-            lin.store_results(results, lin.elts)  # useful?
+            if FLAG_FIX:
+                fault_scenario.fix_all()
+                results = lin.elts.compute_transfer_matrices()  # useful?
+                lin.store_results(results, lin.elts)  # useful?
 
-        linacs.append(lin)
+            linacs.append(lin)
 
-        # Output some info on the quality of the fit
-        end_time = time.monotonic()
-        delta_t = datetime.timedelta(seconds=end_time - start_time)
-        logging.info(f"Elapsed time: {delta_t}")
+            # Output some info on the quality of the fit
+            end_time = time.monotonic()
+            delta_t = datetime.timedelta(seconds=end_time - start_time)
+            logging.info(f"Elapsed time: {delta_t}")
 
-        tw.update_dat_with_fixed_cavities(
-            lin.get('dat_filecontent', to_numpy=False), lin.elts,
-            lin.get('field_map_folder'))
-        data_tab_from_tw = tw.output_data_in_tw_fashion(lin)
-        lw_fit_eval = fault_scenario.evaluate_fit_quality(delta_t)
+            tw.update_dat_with_fixed_cavities(
+                lin.get('dat_filecontent', to_numpy=False), lin.elts,
+                lin.get('field_map_folder'))
+            data_tab_from_tw = tw.output_data_in_tw_fashion(lin)
+            lw_fit_eval = fault_scenario.evaluate_fit_quality(delta_t)
 
-        if SAVE_FIX:
-            lin.files['dat_filepath'] = os.path.join(
-                lin.get('out_lw'), os.path.basename(FILEPATH))
-            output.save_files(lin, data=data_tab_from_tw,
-                              lw_fit_eval=lw_fit_eval)
+            if SAVE_FIX:
+                lin.files['dat_filepath'] = os.path.join(
+                    lin.get('out_lw'), os.path.basename(FILEPATH))
+                output.save_files(lin, data=data_tab_from_tw,
+                                  lw_fit_eval=lw_fit_eval)
 
-        lw_fit_evals.append(lw_fit_eval)
+            lw_fit_evals.append(lw_fit_eval)
 
 # =============================================================================
 # TraceWin
@@ -169,7 +171,7 @@ if __name__ == '__main__':
                 d_bruce = evaluate.bruce_tests(linacs[0], lin)
                 l_bruce.append(d_bruce)
 
-        if FLAG_EVALUATE:
+        if FLAG_FIX and FLAG_EVALUATE:
             for _list, name in zip([l_fred, l_bruce],
                                    ['fred_tests.csv', 'bruce_tests.csv']):
                 out = pd.DataFrame(_list)
@@ -184,5 +186,11 @@ if __name__ == '__main__':
         for str_plot in PLOTS:
             # Plot the reference linac, i-th broken linac and corresponding
             # fixed linac
-            args = (linacs[0], linacs[2 * i + 1], linacs[2 * i + 2])
+            if not FLAG_BREAK:
+                args = (linacs[0], )
+            else:
+                if not FLAG_FIX:
+                    args = (linacs[0], linacs[i + 1])
+                else:
+                    args = (linacs[0], linacs[2 * i + 1], linacs[2 * i + 2])
             plot.plot_preset(str_plot, *args, **kwargs)
