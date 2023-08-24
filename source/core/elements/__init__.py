12f226c17c43bd0d3df9f239ec32a02688f1c883
Adrien P <placais@lpsc.in2p3.fr>
2023-08-24 21:23:38 +0200

    Todo list for the module.

--- source/core/elements/__init__.py
+++ source/core/elements/__init__.py
@@ -2,4 +2,10 @@
 This package holds all the base class :class:`Element` and its derivatived
 classes.
 
+.. todo::
+    Implement a dummy element, that will not be considered but will be kept.
+
+.. todo::
+    ``elem`` in the init should be ``line`` for consistency.
+
 """
