486cfa5a56475d33d1a77a18c71719123d91fbb4
Adrien P <placais@lpsc.in2p3.fr>
2023-08-24 12:56:12 +0200

    Doc but hyperlinks do not work

--- source/beam_calculation/__init__.py
+++ source/beam_calculation/__init__.py
@@ -2,12 +2,12 @@
 This package holds all the modules that are related to computing the
 propagation of the beam in the linac.
 
-:py:class:`BeamCalculator` is an Abstract Base Class to hold such a solver.
+:class:`BeamCalculator` is an Abstract Base Class to hold such a solver.
 :Class:`Envelope1D` and :class:`TraceWin` are the currently implemented solvers
 inheriting from it. They can be created very easily using the :mod:`factory`
 module.
 
-mod:`output` module is used to uniformly store beam properties across all
+:mod:`output` module is used to uniformly store beam properties across all
 solvers.
 
 In order to work, solvers rely on :class:`SingleElementCalculatorParameters` to
