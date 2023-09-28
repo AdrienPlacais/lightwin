df42bd89dec9d6424ffba1d29b2de23dea949ee4
Adrien P <placais@lpsc.in2p3.fr>
2023-09-28 09:43:02 +0200

    More things are performed in dedicated methods. Variables have more consistent names.

--- source/optimisation/algorithms/explorator.py
+++ source/optimisation/algorithms/explorator.py
@@ -44,7 +44,7 @@ class Explorator(OptimisationAlgorithm):
         self.supports_constraints = True
 
     def optimise(self) -> tuple[bool,
-                                SetOfCavitySettings,
+                                SetOfCavitySettings | None,
                                 dict[str, list[float]]]:
         """
         Set up the optimisation and solve the problem.
@@ -53,48 +53,64 @@ class Explorator(OptimisationAlgorithm):
         -------
         success : bool
             Tells if the optimisation algorithm managed to converge.
-        optimized_cavity_settings : SetOfCavitySettings
+        optimized_cavity_settings : SetOfCavitySettings | None
             Best solution found by the optimization algorithm.
         info : dict[str, list[float]]] | None
             Gives list of solutions, corresponding objective, convergence
             violation if applicable, etc.
 
         """
-        self._check_dimensions()
+        is_plottable = self._check_dimensions()
         kwargs = self._algorithm_parameters()
-        variable_mesh, variables_comb = self._generate_combinations(**kwargs)
+        variables_as_mesh, variables_values = self._generate_combinations(
+            **kwargs)
 
-        results = [self._wrapper_residuals(var) for var in variables_comb]
-        objective_values = np.array([res[0] for res in results])
-        are_constraints_ok = np.array([res[1] for res in results])
+        results = [self._wrapper_residuals(var) for var in variables_values]
+        objectives_values = np.array([res[0] for res in results])
+        constraints_values = np.array([res[1] for res in results])
 
-        objective_mesh = self._results_as_mesh(objective_values, **kwargs)
-        constraint_mesh = self._results_as_mesh(are_constraints_ok, **kwargs)
+        objectives_as_mesh = self._array_of_values_to_mesh(objectives_values,
+                                                           **kwargs)
+        constraints_as_mesh = self._array_of_values_to_mesh(constraints_values,
+                                                            **kwargs)
 
-        axes = self._output_some_info(variable_mesh, objective_mesh,
-                                      constraint_mesh)
-
-        best_idx = np.nanargmin(objective_values)
-        info = {'X': variables_comb[best_idx],
-                'F': objective_values[best_idx]
+        best_solution, best_objective = self._take_best_solution(
+            variables_values,
+            objectives_values,
+            criterion="minimize norm of objective"
+        )
+        info = {'X': best_solution,
+                'hist_X': variables_values,
+                'F': best_objective,
+                'hist_F': objectives_values,
+                'hist_G': constraints_values,
                 }
 
-        self._add_the_best_point(axes, info['X'], info['F'])
+        if is_plottable:
+            axes = self._plot_design_space(variables_as_mesh,
+                                           objectives_as_mesh,
+                                           constraints_as_mesh)
+            self._add_the_best_point(axes, info['X'], info['F'])
 
         optimized_cavity_settings = self._create_set_of_cavity_settings(
             info['X'])
         return True, optimized_cavity_settings, info
 
-    def _check_dimensions(self) -> None:
+    def _check_dimensions(self) -> bool:
         """Check that we have proper number of vars and objectives."""
         if self.n_obj != 1:
             logging.warning("The number of objectives is different from 1. "
                             "Hence I will simply plot the norm of objectives.")
-        assert self.n_var == 2, "Wrong number of variables, cannot represent."
+        if self.n_var != 2:
+            logging.warning("Wrong number of variables. Impossible to plot "
+                            "it, but I will compute all possible solutions "
+                            "anyway.")
+            return False
+        return True
 
     def _algorithm_parameters(self) -> dict:
         """Create the ``kwargs`` for the optimisation."""
-        kwargs = {'n_points': 10}
+        kwargs = {'n_points': 100}
         return kwargs
 
     def _generate_combinations(self, n_points: int = 10, **kwargs
@@ -126,15 +142,54 @@ class Explorator(OptimisationAlgorithm):
             and constraints_evaluations[1] < 0.
         return np.linalg.norm(residuals), is_ok
 
-    def _results_as_mesh(self, objective_values: np.ndarray,
+    def _array_of_values_to_mesh(self, objective_values: np.ndarray,
                          n_points: int = 10, **kwargs) -> np.ndarray:
         """Reformat the results for plotting purposes."""
         return objective_values.reshape((n_points, n_points)).T
 
-    def _output_some_info(self,
-                          variable_mesh: np.ndarray,
-                          objective_mesh: np.ndarray,
-                          constraint_mesh: np.ndarray) -> mplot3d.Axes3D:
+    def _take_best_solution(self,
+                            variable_comb: np.ndarray,
+                            objective_values: np.ndarray,
+                            criterion: str,
+                            ) -> tuple[np.ndarray | None, np.ndarray | None]:
+        """Take the "best" of the calculated solutions.
+
+        Parameters
+        ----------
+        variable_comb : np.ndarray
+            All the set of variables (cavity parameters) that were tried.
+        objective_values : np.ndarray
+            The values of the objective corresponding to ``variable_comb``.
+        criterion : {'minimize norm of objective', }
+            Name of the criterion that will determine which solution is the
+            "best". Only one is implemented for now, may add others in the
+            future.
+
+        Returns
+        -------
+        best_solution : np.ndarray | None
+            "Best" solution.
+        best_objective : np.ndarray | None
+            Objective values corresponding to ``best_solution``.
+
+        """
+        if criterion == 'minimize norm of objective':
+            norm_of_objective = objective_values
+            if len(norm_of_objective.shape) > 1:
+                norm_of_objective = np.linalg.norm(norm_of_objective, axis=1)
+            best_idx = np.nanargmin(norm_of_objective)
+            best_solution = variable_comb[best_idx]
+            best_objective = objective_values[best_idx]
+            return best_solution, best_objective
+
+        logging.error(f"{criterion = } not implemented. Please check your "
+                      "input.")
+        return None, None
+
+    def _plot_design_space(self,
+                           variable_mesh: np.ndarray,
+                           objective_mesh: np.ndarray,
+                           constraint_mesh: np.ndarray) -> mplot3d.Axes3D:
         """Plot the design space."""
         fig = plt.figure(30)
         axes = fig.add_subplot(projection='3d')
