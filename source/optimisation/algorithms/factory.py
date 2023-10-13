8d0615e9e5b3cb97ec1bbffec97c98e4a322a552
Adrien P <placais@lpsc.in2p3.fr>
2023-10-13 08:55:09 +0200

    Factory created, but not used yet.

---
+++ source/optimisation/algorithms/factory.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 17:30:58 2023.
+
+@author: placais
+
+This module holds a factory method to create the desired
+:class:`OptimisationAlgorithm`.
+
+"""
+from typing import Any, Callable
+from abc import ABCMeta
+import logging
+from functools import partial
+
+from failures.fault import Fault
+from optimisation.algorithms.algorithm import OptimisationAlgorithm
+from optimisation.algorithms.least_squares import LeastSquares
+from optimisation.algorithms.least_squares_penalty import LeastSquaresPenalty
+from optimisation.algorithms.nsga import NSGA
+from optimisation.algorithms.downhill_simplex import DownhillSimplex
+from optimisation.algorithms.downhill_simplex_penalty import \
+    DownhillSimplexPenalty
+from optimisation.algorithms.differential_evolution import \
+    DifferentialEvolution
+from optimisation.algorithms.explorator import Explorator
+
+
+ALGORITHMS: dict[str, ABCMeta] = {
+    'least_squares': LeastSquares,
+    'least_squares_penalty': LeastSquaresPenalty,
+    'nsga': NSGA,
+    'downhill_simplex': DownhillSimplex,
+    'nelder_mead': DownhillSimplex,
+    'nelder_mead_penalty': DownhillSimplexPenalty,
+    'differential_evolution': DifferentialEvolution,
+    'explorator': Explorator,
+    'experimental': Explorator,
+    }
+
+
+def create_optimisation_algorithm(
+        name: str,
+        fault: Fault,
+        beam_calculator_run_with_this: Callable,
+        **kwargs: Any
+        ) -> OptimisationAlgorithm:
+    """
+    Create the proper :class:`OptimisationAlgorithm` instance.
+
+    Parameters
+    ----------
+    name : str
+        Name of the desired optimisation algorithm.
+    fault : Fault
+        Fault that will be compensated by the optimisation algorithm.
+    compute_beam_propagation : Callable
+        Function that takes in a set of cavity settings and a list of elements,
+        computes the beam propagation with these, and returns a simulation
+        output.
+
+    Returns
+    -------
+    beam_calculators : OptimisationAlgorithm
+        Proper optimisation algorithm.
+
+    """
+    new_kwargs = _optimisation_algorithm_kwargs(fault,
+                                                beam_calculator_run_with_this)
+    _check_common_keys(kwargs, new_kwargs)
+    kwargs = new_kwargs | kwargs
+
+    algorithm_base_class = ALGORITHMS[name]
+    algorithm: OptimisationAlgorithm = algorithm_base_class(**kwargs)
+    return algorithm
+
+
+def _optimisation_algorithm_kwargs(
+        fault: Fault,
+        beam_calculator_run_with_this: Callable,
+        ) -> dict[str, Any]:
+    """Set default arguments to instantiate the optimisation algorithm.
+
+    The arguments that are defined in FaultScenario and are given to the
+    :func:`create_optimisation_algorithm` will override the ones here if they
+    are multiply defined.
+
+    Parameters
+    ----------
+    fault : Fault
+        Fault that will be compensated by the optimisation algorithm.
+    compute_beam_propagation : Callable
+        Function that takes in a set of cavity settings and a list of elements,
+        computes the beam propagation with these, and returns a simulation
+        output.
+
+    Returns
+    -------
+    new_kwargs : dict[str, Any]
+        A dictionary of keyword arguments for the
+        :class:`OptimisationAlgorithm`.
+
+    """
+    compute_beam_propagation = partial(beam_calculator_run_with_this,
+                                       elts=fault.elts)
+    new_kwargs: dict[str, Any] = {
+        "compute_beam_propagation": compute_beam_propagation,
+        "objectives": fault.objectives,
+        "compute_residuals": fault.compute_residuals,
+        "compensating_cavities": fault.compensating_cavities,
+        "elts": fault.elts,
+        "variables": fault.variables,
+        "constraints": fault.constraints,
+        "compute_constraints": fault.compute_constraints,
+        }
+    return new_kwargs
+
+
+def _check_common_keys(kwargs: dict[str, Any], new_kwargs: dict[str, Any]
+                       ) -> None:
+    """Check keys that are common between the two dictionaries.
+
+    Parameters
+    ----------
+    kwargs : dict[str, Any]
+        kwargs as defined in the :class:`FaultScenario` (they have precedence).
+    new_kwargs : [str, Any]
+        kwargs as defined in the :func:`_optimisation_algorithm_kwargs` (they
+        will be overriden as they are considered as "default" or "fallback"
+        values).
+
+    """
+    keys = set(kwargs.keys())
+    new_keys = set(new_kwargs.keys())
+    common_keys = keys.intersection(new_keys)
+    if len(common_keys) > 0:
+        logging.info("The following OptimisationAlgorithm arguments are "
+                     "set both in FaultScenario (kwargs) and in "
+                     "optimisation.algorithms.factory (new_kwargs). We use"
+                     " the ones from FaultScenario.\n"
+                     f"{common_keys = })")
