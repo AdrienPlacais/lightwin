a1ff2c63a80cf36aa4a70cab829ce9571b9011b4
Adrien P <placais@lpsc.in2p3.fr>
2023-10-13 16:41:12 +0200

    Added a docstring for the subpackage.

---
+++ source/config/failures/__init__.py
@@ -0,0 +1,7 @@
+"""
+In this subpackage we define the :mod:`failures` configuration checkers.
+
+Related documentations are stored in the subpackages of :mod:`failures`, not in
+the subpackages of :mod:`config.failures`.
+
+"""
