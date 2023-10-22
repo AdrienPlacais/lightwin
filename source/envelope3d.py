c9929d96ab99b17fdb664d0467e526e3fc903100
Adrien P <placais@lpsc.in2p3.fr>
2023-10-22 17:46:00 +0200

    With small new tests.

--- source/envelope3d.py
+++ source/envelope3d.py
@@ -114,13 +114,81 @@ if __name__ == '__main__':
 # =============================================================================
 # Compute cumulated transfer matrix
 # =============================================================================
-import numpy as np
-sim = accelerators[0].simulation_outputs[solv1]
-individual_transfer_matrices = sim.transfer_matrix
-n_points = len(individual_transfer_matrices)
-cumulated_transfer_matrices = np.empty((n_points, 6, 6))
-cumulated_transfer_matrices[0] = individual_transfer_matrices[0]
-for i in range(1, n_points):
-    cumulated_transfer_matrices[i] = \
-        individual_transfer_matrices[i] \
-        @ cumulated_transfer_matrices[i - 1]
+    import numpy as np
+
+    # Get calculated transfer matrices
+    sim = accelerators[0].simulation_outputs[solv1]
+    individual_transfer_matrices = sim.transfer_matrix
+    n_points = len(individual_transfer_matrices)
+    cumulated_transfer_matrices = np.empty((n_points, 6, 6))
+    cumulated_transfer_matrices[0] = individual_transfer_matrices[0]
+    for i in range(1, n_points):
+        cumulated_transfer_matrices[i] = \
+            individual_transfer_matrices[i] \
+            @ cumulated_transfer_matrices[i - 1]
+    pos = sim.get('z_abs')
+
+    # Get reference transfer matrices
+    from tracewin_utils import load
+    path = '/home/placais/LightWin/data/from_tracewin_examples/results/Transfer_matrix1.dat'
+    _, ref_pos, ref_cumulated_transfer_matrices = load.transfer_matrices(path)
+
+    # Also interpolate LightWin on TraceWin
+    from util.helper import resample
+    n_ref_points = ref_pos.shape[0]
+    interp_cumulated = np.empty((n_ref_points, 6, 6))
+    for i in range(6):
+        for j in range(6):
+            _, interp_cumulated[:, i, j], _, _ = resample(
+                x_1=pos[1:],
+                y_1=cumulated_transfer_matrices[:, i, j],
+                x_2=ref_pos,
+                y_2=ref_cumulated_transfer_matrices[:, i, j])
+
+    # Compute diff between transfer matrices
+    rel_diff = 100. * (interp_cumulated - ref_cumulated_transfer_matrices) \
+        / ref_cumulated_transfer_matrices
+    normal_diff = interp_cumulated - ref_cumulated_transfer_matrices
+
+    # Now a plot
+    from visualization.plot import (_create_fig_if_not_exists,
+                                    _plot_structure,
+                                    )
+    i = 1
+    j = 1
+    fig, axes = _create_fig_if_not_exists(axnum=3,
+                                          sharex=True,
+                                          num=25,
+                                          clean_fig=True)
+    axes[0].plot(ref_pos,
+                 ref_cumulated_transfer_matrices[:, i, j],
+                 label='Reference')
+    axes[0].plot(pos[1:],
+                 cumulated_transfer_matrices[:, i, j],
+                 label='Calculated')
+    axes[0].legend()
+    axes[0].grid(True)
+    axes[0].set_ylabel(f'M {i} {j}')
+
+    rel_plot = True
+    if rel_plot:
+        axes[1].plot(ref_pos, rel_diff[:, i, j])
+        axes[1].set_ylabel('Rel diff %')
+    else:
+        axes[1].plot(ref_pos, normal_diff[:, i, j])
+        axes[1].set_ylabel('Abs diff')
+
+    axes[1].grid(True)
+    _plot_structure(accelerators[0], axes[2])
+
+    tw_energy = 80.668954
+    my_energy = sim.get('w_kin', elt='last', pos='out')
+    print(f"Rel error on energy: {100. * (tw_energy - my_energy) / tw_energy}%")
+
+    tw_beta = 0.38996284
+    my_beta = sim.get('beta', elt='last', pos='out')
+    print(f"Rel error on beta: {100. * (tw_beta - my_beta) / tw_beta}%")
+
+    tw_phase = 54985.46
+    my_phase = sim.get('phi_abs', elt='last', pos='out', to_deg=True)
+    print(f"Rel error on phi: {100. * (tw_phase - my_phase) / tw_phase}%")
