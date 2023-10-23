a654571bfc55fe08e971d91501ee9c69fe8cdcb0
Adrien P <placais@lpsc.in2p3.fr>
2023-10-23 11:24:16 +0200

    It is now possible to insert eye matrix or the previous cumulated transfer matrix at the start of TransferMatrix.cumulated.

--- source/core/transfer_matrix.py
+++ source/core/transfer_matrix.py
@@ -6,10 +6,6 @@ Hold the transfer matrix along the linac.
 .. todo::
     Check if it can be more efficient. Maybe store R_xx, R_yy, R_zz separately?
 
-.. todo::
-    Handle when treating sub-linacs. First cumulated transfer matrix then sould
-    not be ``eye`` matrix.
-
 """
 import logging
 import numpy as np
@@ -36,7 +32,7 @@ class TransferMatrix:
     def __init__(self,
                  individual: np.ndarray | list[np.ndarray] | None = None,
                  cumulated: np.ndarray | None = None,
-                 insert_eye_matrix: bool = True,
+                 first_cumulated_transfer_matrix: np.ndarray | None = None,
                  ) -> None:
         """Create the object and compute the cumulated transfer matrix.
 
@@ -48,9 +44,11 @@ class TransferMatrix:
         cumulated : np.ndarray | None, optional
             Cumulated transfer matrices. The default is None, in which case the
             ``individual`` transfer matrices must be given.
-        insert_eye_matrix : bool, optional
-            If an eye matrix should be inserted at the first position of
-            ``cumulated``. The default is True.
+        first_cumulated_transfer_matrix : np.ndarray
+            First transfer matrix. The default is None, in which case it will
+            be converted to the eye matrix -- corresponds to the study of a
+            full linac. It should be the cumulated transfer matrix of the
+            previous portion of linac otherwise.
 
         """
         if isinstance(individual, list):
@@ -59,26 +57,35 @@ class TransferMatrix:
         self.individual: np.ndarray
         if individual is not None:
             self.individual = individual
-            is_3d, n_points, cumulated = self._init_from_individual(individual)
+            is_3d, n_points, cumulated = self._init_from_individual(
+                individual,
+                first_cumulated_transfer_matrix)
 
         else:
-            is_3d, n_points, cumulated = self._init_from_cumulated(cumulated)
+            is_3d, n_points, cumulated = self._init_from_cumulated(
+                cumulated,
+                first_cumulated_transfer_matrix)
 
         self.is_3d = is_3d
         self.n_points = n_points
 
-        if insert_eye_matrix:
-            cumulated = self._insert_eye_transfer_matrix_at_start(cumulated)
         self.cumulated = cumulated
 
-    def _init_from_individual(self, individual: np.ndarray
-                              ) -> tuple[bool, int, np.ndarray]:
+    def _init_from_individual(
+            self,
+            individual: np.ndarray,
+            first_cumulated_transfer_matrix: np.ndarray | None,
+            ) -> tuple[bool, int, np.ndarray]:
         """Compute cumulated transfer matrix from individual.
 
         Parameters
         ----------
         individual : np.ndarray
             Individual transfer matrices along the linac.
+        first_cumulated_transfer_matrix : np.ndarray | None
+            First transfer matrix. It should be None if we study a linac
+            from the start (``z_pos == 0.``), and should be the cumulated
+            transfer matrix of the previous linac portion otherwise.
 
         Returns
         -------
@@ -90,19 +97,43 @@ class TransferMatrix:
             Cumulated transfer matrices.
 
         """
+        n_points = individual.shape[0] + 1
         is_3d = self._determine_if_is_3d(individual)
-        n_points = individual.shape[0]
-        cumulated = self._compute_cumulated(is_3d, n_points)
+        if is_3d:
+            shape = (n_points, 6, 6)
+        else:
+            shape = (n_points, 2, 2)
+
+        if first_cumulated_transfer_matrix is None:
+            first_cumulated_transfer_matrix = np.eye(shape[1])
+
+        cumulated = self._compute_cumulated(first_cumulated_transfer_matrix,
+                                            shape,
+                                            is_3d,
+                                            n_points)
         return is_3d, n_points, cumulated
 
-    def _init_from_cumulated(self, cumulated: np.ndarray | None
-                             ) -> tuple[bool, int, np.ndarray]:
+    def _init_from_cumulated(
+            self,
+            cumulated: np.ndarray | None,
+            first_cumulated_transfer_matrix: np.ndarray | None,
+            tol: float = 1e-8
+            ) -> tuple[bool, int, np.ndarray]:
         """Check that the given cumulated matrix is valid.
 
         Parameters
         ----------
         cumulated : np.ndarray
             Cumulated transfer matrices along the linac.
+        first_cumulated_transfer_matrix : np.ndarray | None
+            The first of the cumulated transfer matrices. The default is None,
+            in which case we insert an eye matrix at the first position.
+            Otherwise, we insert the given matrix. This insertion is skipped if
+            the matrix we try to insert is already the first transfer matrix.
+        tol : float, optional
+            The max allowed difference between ``cumulated`` and
+            ``first_cumulated_transfer_matrix`` when determining if they are
+            the same or not.
 
         Returns
         -------
@@ -121,18 +152,37 @@ class TransferMatrix:
             raise IOError("Wrong input")
         is_3d = self._determine_if_is_3d(cumulated)
         n_points = cumulated.shape[0]
+
+        if first_cumulated_transfer_matrix is None:
+            first_cumulated_transfer_matrix = np.eye(6)
+
+        if (np.abs(cumulated[0]
+                   - first_cumulated_transfer_matrix)).any() > tol:
+            n_points += 1
+            cumulated = np.vstack((first_cumulated_transfer_matrix[np.newaxis],
+                                   cumulated))
+
         return is_3d, n_points, cumulated
 
     def _compute_cumulated(self,
+                           first_cumulated_transfer_matrix: np.ndarray,
+                           shape: tuple[int, int, int],
                            is_3d: bool,
                            n_points: int) -> np.ndarray:
         """Compute cumulated transfer matrix from individual.
 
         Parameters
         ----------
+        first_cumulated_transfer_matrix : np.ndarray
+            First transfer matrix. It should be eye matrix if we study a linac
+            from the start (``z_pos == 0.``), and should be the cumulated
+            transfer matrix of the previous linac portion otherwise.
+        shape : tuple[int, int, int]
+            Shape of the output ``cumulated`` array.
         is_3d : bool
-            If the simulation is in 3D or not. If it is in 1D only, we fill
-            everything but the [z-dp/p] array with NaN.
+            If the simulation is in 3D or not.
+        n_points : int
+            Number of mesh points along the linac.
 
         Returns
         -------
@@ -143,21 +193,17 @@ class TransferMatrix:
             I think the 3D/1D handling may be smarter?
 
         """
-        fill_value = np.NaN
-        shape = (n_points, 2, 2)
-        if is_3d:
-            fill_value = 0.
-            shape = (n_points, 6, 6)
-        cumulated = np.full(shape, fill_value)
+        cumulated = np.full(shape, np.NaN)
+        cumulated[0] = first_cumulated_transfer_matrix
 
-        cumulated[0] = self.individual[0]
-        for i in range(1, n_points):
-            cumulated[i] = self.individual[i] @ cumulated[i - 1]
+        for i in range(n_points - 1):
+            cumulated[i + 1] = self.individual[i] @ cumulated[i]
+
         if is_3d:
             return cumulated
 
         cumulated_1d = cumulated
-        cumulated = np.full((n_points, 6, 6), fill_value)
+        cumulated = np.full((n_points, 6, 6), np.NaN)
         cumulated[:, 4:, 4:] = cumulated_1d
         return cumulated
 
@@ -170,13 +216,6 @@ class TransferMatrix:
         logging.error(f"The individual transfer matrices have shape {shape}, "
                       "while (2, 2) (1D) or (6, 6) (3D) are expected.")
         raise IOError("Wrong dimensions for given transfer matrices.")
-
-    def _insert_eye_transfer_matrix_at_start(self, array: np.ndarray
-                                             ) -> np.ndarray:
-        """Insert eye matrix at first position of ``array``."""
-        self.n_points += 1
-        eye = np.eye(6)
-        return np.vstack((eye[np.newaxis], array))
 
     @property
     def r_xx(self) -> np.ndarray:
