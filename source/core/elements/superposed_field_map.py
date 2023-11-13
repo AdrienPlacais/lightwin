e0c390a6e32ed6c461c8f0d2a36f0f74cf3d6b50
Adrien P <placais@lpsc.in2p3.fr>
2023-11-13 17:20:58 +0100

    updated todo list

--- source/core/elements/superposed_field_map.py
+++ source/core/elements/superposed_field_map.py
@@ -6,6 +6,9 @@
     The initialisation of this class is particular, as it does not correspond
     to a specific line of the ``.dat`` file.
 
+.. todo::
+    Maybe move this to the field_map package once it is implemented
+
 """
 from core.elements.element import Element
 from core.elements.field_maps.field_map import FieldMap
