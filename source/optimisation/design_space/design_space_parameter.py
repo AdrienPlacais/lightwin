969382c8ee232d67e85c5f27e470c00c470cced1
Adrien P <placais@lpsc.in2p3.fr>
2023-11-17 09:42:34 +0100

    object from which variable and constraint will inherit

---
+++ source/optimisation/design_space/design_space_parameter.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Create a base class for :class:`.Variable` and :class:`.Constraint`."""
+from abc import ABC
+from dataclasses import dataclass
+from typing import Sequence
+import numpy as np
+
+from util.dicts_output import markdown
+
+
+@dataclass
+class DesignSpaceParameter(ABC):
+    """Hold a single variable or constraint."""
+
+    name: str
+    element_name: str
+    limits: tuple[float, float]
+
+    def __post_init__(self):
+        """Convert values in deg for output if it is angle."""
+        self._to_deg = False
+        self._to_numpy = False
+
+    @property
+    def _fmt_limits(self) -> Sequence[float]:
+        """Limits but with a better output."""
+        if 'phi' in self.name:
+            return np.rad2deg(self.limits)
+        return self.limits
+
+    @property
+    def _fmt_x_0(self) -> float:
+        """Initial value but with a better output."""
+        assert hasattr(self, 'x_0'), "This design space parameter has no " \
+            "attribute x_0. Maybe you took a Contraint for a Variable?"
+        x_0 = getattr(self, 'x_0')
+        if 'phi' in self.name:
+            return np.rad2deg(x_0)
+        return x_0
+
+    def __str__(self) -> str:
+        """Output constraint name and limits."""
+        out = f"{markdown[self.name]:25} | {self.element_name:15} | "
+        if hasattr(self, 'x_0'):
+            out += f"{self._fmt_x_0:>8.3f} | "
+        else:
+            out += "{' ':<8} | "
+        out += f"limits={self._fmt_limits[0]:>9.3f} "
+        out += f"{self._fmt_limits[1]:>9.3f}"
+
+        return out
+
+    @staticmethod
+    def header_of__str__() -> str:
+        """Give information on what :func:`__str__` is about."""
+        header = f"{'Constraint':<25} | {'Element':<15} | {'x_0':<8} | "
+        header += f"{'Lower lim':<9} | {'Upper lim':<9}"
+        return header
+
