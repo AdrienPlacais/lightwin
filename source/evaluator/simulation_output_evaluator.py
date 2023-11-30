170bb00ae6eaba43343e9ee80ab00d2ef6f53677
Adrien P <placais@lpsc.in2p3.fr>
2023-11-30 13:02:35 +0100

    clean

--- source/evaluator/simulation_output_evaluator.py
+++ source/evaluator/simulation_output_evaluator.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-We define an object to evaluate quality of a set of cavity settings.
+Define an object to evaluate quality of a set of cavity settings.
 
 .. note::
     We do not directly evaluate a :class:`.SetOfCavitySettings` though, but
     rather a :class:`.SimulationOutput`.
 
 """
+from abc import ABC
+from dataclasses import dataclass
+from functools import partial
 import logging
 import os.path
-from typing import Callable, Any
-from functools import partial
-from dataclasses import dataclass
-from abc import ABC
+from pathlib import Path
+from typing import Any, Callable
 
 import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.axes._axes import Axes
 
-from beam_calculation.simulation_output.simulation_output import \
-    SimulationOutput
+from beam_calculation.simulation_output.simulation_output import (
+    SimulationOutput,
+)
 from evaluator.post_treaters import do_nothing
 from util.helper import resample
 from visualization import plot
@@ -27,18 +31,16 @@ from visualization import plot
 # =============================================================================
 # Helpers
 # =============================================================================
-def _need_to_resample(value: np.ndarray | float, ref_value: np.ndarray | float
+def _need_to_resample(value: np.ndarray | float,
+                      ref_value: np.ndarray | float,
                       ) -> bool:
-    """Determine if we need to resample `value` or `ref_value`."""
-    for val in [ref_value, value]:
-        if isinstance(val, float):
-            return False
-        if val.shape == ():
-            return False
-
+    """Determine if we need to resample ``value`` or ``ref_value``."""
+    if isinstance(value, float) or isinstance(ref_value, float):
+        return False
+    if value.shape == () or ref_value.shape == ():
+        return False
     if value.shape == ref_value.shape:
         return False
-
     return True
 
 
@@ -46,11 +48,11 @@ def _return_value_should_be_plotted(partial_function: Callable) -> bool:
     """
     Determine if keyword 'to_plot' was passed and is True.
 
-    This function only works on functions defined by `functools.partial`. If it
-    is not (lambda function, "classic" function), we consider that the plotting
-    was not desired.
-    Then we check if the 'to_plot' keyword was given in the partial definition,
-    and if it is not we also consider that the plot was not wanted.
+    This function only works on functions defined by ``functools.partial``. If
+    it is not (lambda function, "classic" function), we consider that the
+    plotting was not desired.
+    We check if the 'to_plot' keyword was given in the partial definition, and
+    if it is not we also consider that the plot was not wanted.
 
     """
     if not isinstance(partial_function, partial):
@@ -92,58 +94,57 @@ class SimulationOutputEvaluator(ABC):
 
     Arguments
     ---------
-    value_getter : Callable[SimulationOutput, Any]
-        A function that takes the `SimulationOutput` under study as argument,
+    value_getter : Callable[[SimulationOutput], Any]
+        A function that takes the simulation output under study as argument,
         and returns the value to be studied.
     ref_value_getter : Callable[[SimulationOutput, SimulationOutput],
                                  Any] | None, optional
-        A function that takes the reference `SimulationOutput` and the
-        `SimulationOutput` under study as arguments, and returns the reference
-        value. In general, only one of the arguments will be used. The default
-        is None.
+        A function that takes the reference simulation ouput and the simulation
+        output under study as arguments, and returns the reference value. In
+        general, only one of the arguments will be used. The default is None.
     ref_simulation_output : SimulationOutput | None, optional
-        The SimulationOutput of a nominal `Accelerator`. It is up to the user
-        to verify that the `BeamCalculator` is the same between the reference
-        and the fixed `SimulationOutput`. The default value is None.
+        The SimulationOutput of a nominal :class:`.Accelerator`. It is up to
+        the user to verify that the :class:`.BeamCalculator` is the same
+        between the reference and the fixed :class:`.SimulationOutput`. The
+        default is None.
     post_treaters: tuple[Callable[[np.ndarray | float, np.ndarray | float],
                                   np.ndarray | float]], optional
-        A tuple of functions called one after each other. They take `value` as
-        first argument, `ref_value` as second argument. It returns an updated
-        `value`, which is given to the next function in the tuple. The default
-        is (`_do_nothing`,).
-    tester : Callable[np.ndarray | float, bool] | None, optional
-        A function that takes `value` after post_treatment and returns a
-        boolean. The default is None.
+        A tuple of functions called one after each other. They take ``value``
+        as first argument, ``ref_value`` as second argument. They return an
+        updated ``value``, which is given to the next function in the tuple.
+        The default is ``(:fun:`_do_nothing`,)``.
+    tester : Callable[[np.ndarray | float], bool | float | None] | None, optional
+        A function that takes ``value`` after post treatment and test is. The
+        default is None.
     fignum : int | None, optional
         The Figure number. The default is None, in which case no plot is
         produced.
-    descriptor : str | None, optional
-        A sentence or two to describe what the test is about.
+    descriptor : str, optional
+        A sentence or two to describe what the test is about. The default is an
+        empty string.
     markdown : str | None, optional
         A markdown name for this quantity, used in plots. The default is None.
 
     """
 
-    value_getter: Callable[SimulationOutput, Any]
+    value_getter: Callable[[SimulationOutput], Any]
+    ref_simulation_output: SimulationOutput
     ref_value_getter: Callable[[SimulationOutput, SimulationOutput],
                                Any] | None = None
 
-    ref_simulation_output: SimulationOutput | None = None
+    post_treaters: tuple[Callable[[np.ndarray | float, np.ndarray | float],
+                                  np.ndarray | float], ...
+                         ] = (do_nothing,)
 
-    post_treaters: tuple[Callable[
-        [np.ndarray | float, np.ndarray | float],
-        np.ndarray | float]
-    ] = (do_nothing,)
+    tester: Callable[[np.ndarray | float], bool | float | None] | None = None
 
-    tester: Callable[np.ndarray | float, bool] | None = None
+    descriptor: str = ''
+    markdown: str = 'test'
 
-    descriptor: str | None = None
-    markdown: str | None = None
-
-    plt_kwargs: dict | None = None
+    plt_kwargs: dict[str, Any] | None = None
 
     def __post_init__(self):
-        """Check inputs, create plot if a `fignum` was provided."""
+        """Check inputs, create plot if a ``fignum`` was provided."""
         if self.descriptor is None:
             logging.warning("No descriptor was given for this evaluator, which"
                             " may be confusing in the long run.")
@@ -157,13 +158,11 @@ class SimulationOutputEvaluator(ABC):
                             "for the best...")
             self.post_treaters = tuple(self.post_treaters)
 
-        if self.markdown is None:
-            self.markdown = 'test'
-
         if self.plt_kwargs is None:
             self.plt_kwargs = {}
-        self._fig: object | None = None
-        self.main_ax: object | None = None
+
+        self._fig: Figure | None = None
+        self.main_ax: Axes | None = None
         self._create_plot(**self.plt_kwargs)
 
     def __repr__(self) -> str:
@@ -189,6 +188,7 @@ class SimulationOutputEvaluator(ABC):
                           "quantity is probably a mismatch_factor, which "
                           "was interpolated. Returning None.")
             value = None
+
         if value is None:
             logging.error(f"A value misses in test: {self}. Skipping...")
             return None
@@ -220,7 +220,9 @@ class SimulationOutputEvaluator(ABC):
         self._save_plot(simulation_output.out_path, **self.plt_kwargs)
         return test
 
-    def _create_plot(self, fignum: int | None = None, **kwargs) -> None:
+    def _create_plot(self,
+                     fignum: int | None = None,
+                     **kwargs) -> None:
         """Prepare the plot."""
         if fignum is None:
             return
@@ -236,7 +238,9 @@ class SimulationOutputEvaluator(ABC):
         self.main_ax = axx[0]
         # see what are the kwargs for _create_fig_if_not_exists...
 
-    def _add_a_value_plot(self, z_data: np.ndarray, value: np.ndarray | float
+    def _add_a_value_plot(self,
+                          z_data: np.ndarray,
+                          value: np.ndarray | float
                           ) -> None:
         """Add (treated) data to the plot."""
         assert self.main_ax is not None
@@ -245,22 +249,29 @@ class SimulationOutputEvaluator(ABC):
             return
         self.main_ax.plot(z_data, value)
 
-    def _add_a_limit_plot(self, z_data: np.ndarray,
-                          limit: tuple[np.ndarray | float]) -> None:
+    def _add_a_limit_plot(self,
+                          z_data: np.ndarray,
+                          limit: tuple[np.ndarray | float,
+                                       np.ndarray | float]) -> None:
         """Add limits to the plot."""
         assert self.main_ax is not None
 
         for lim in limit:
             if isinstance(lim, float) or lim.shape == ():
-                self.main_ax.axhline(lim, xmin=z_data[0], xmax=z_data[-1],
+                self.main_ax.axhline(lim,
+                                     xmin=z_data[0],
+                                     xmax=z_data[-1],
                                      c='r', ls='--', lw=5)
                 continue
             self.main_ax.plot(z_data, lim)
 
-    def _save_plot(self, out_path: str | None, fignum: int | None = None,
-                   savefig: bool = False, **kwargs
+    def _save_plot(self,
+                   out_path: Path,
+                   fignum: int | None = None,
+                   savefig: bool = False,
+                   **kwargs
                    ) -> None:
-        """Save the figure if asked, and if `out_path` is defined."""
+        """Save the figure if asked, and if ``out_path`` is defined."""
         if not savefig or self._fig is None:
             return
 
@@ -272,5 +283,5 @@ class SimulationOutputEvaluator(ABC):
             return
 
         filename = f"simulation_output_evaluator_{fignum}.png"
-        filepath = os.path.join(out_path, filename)
+        filepath = Path(out_path, filename)
         plot._savefig(self._fig, filepath)
