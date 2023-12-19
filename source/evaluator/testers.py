5fdda3cf22228f38dc51978765e7f8198a1a3731
Adrien P <placais@lpsc.in2p3.fr>
2023-12-19 13:21:01 +0100

    style

--- source/evaluator/testers.py
+++ source/evaluator/testers.py
@@ -20,21 +20,22 @@ def value_is_within_limits(treated_value: np.ndarray | float,
 
 
 def value_is_above(treated_value: np.ndarray | float,
-                   lower_limit: np.ndarray | float, **kwargs: bool
-                   ) -> bool:
+                   lower_limit: np.ndarray | float,
+                   **kwargs: bool) -> bool:
     """Test if the given value is above a threshold."""
     return np.all(treated_value > lower_limit)
 
 
 def value_is_below(treated_value: np.ndarray | float,
-                   upper_limit: np.ndarray | float, **kwargs: bool
-                   ) -> bool:
+                   upper_limit: np.ndarray | float,
+                   **kwargs: bool) -> bool:
     """Test if the given value is below a threshold."""
     return np.all(treated_value < upper_limit)
 
 
 def value_is(treated_value: np.ndarray | float,
-             objective_value: np.ndarray | float, tol: float = 1e-10,
+             objective_value: np.ndarray | float,
+             tol: float = 1e-10,
              **kwargs: bool) -> bool:
-    """Test if the value equals `objective_value`."""
+    """Test if the value equals ``objective_value`."""
     return np.all(np.abs(treated_value - objective_value) < tol)
