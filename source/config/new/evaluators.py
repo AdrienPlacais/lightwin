a2a1b89d22bf78aca0242e4ee6258d2a40a2a815
Adrien P <placais@lpsc.in2p3.fr>
2024-01-20 12:03:14 +0100

    this one was easy

---
+++ source/config/new/evaluators.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Define the functions to check that :class:`.Evaluator` will initialize."""
+
+
+IMPLEMENTED_EVALUATORS = (
+    "no power loss",
+    "transverse eps_x shall not grow too much",
+    "transverse eps_y shall not grow too much",
+    "longitudinal eps shall not grow too much",
+    "max of 99percent transverse eps_x shall not be too high",
+    "max of 99percent transverse eps_y shall not be too high",
+    "max of 99percent longitudinal eps shall not be too high",
+    "transverse eps at end",
+    "mismatch factor at end",
+    "transverse mismatch factor at end",
+)  #:
+
+
+def test(beam_calc_post: list[str], **evaluators_kw: str) -> None:
+    """Check that desired evaluators are implemented."""
+    for evaluator_name in beam_calc_post:
+        assert evaluator_name in IMPLEMENTED_EVALUATORS
+
+
+def add_some_values(evaluators_kw: dict) -> None:
+    """Do nothing."""
+    pass
