87d05519df2ecb148a4aa98a170c3a6e01890ad2
Adrien P <placais@lpsc.in2p3.fr>
2024-04-09 15:44:05 +0200

    black autoformat

--- source/failures/fault_scenario.py
+++ source/failures/fault_scenario.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-This module holds a list-based class holding all the :class:`.Fault` to fix.
+"""Define a list-based class holding all the :class:`.Fault` to fix.
 
 We also define :func:`fault_scenario_factory`, a factory function creating all
 the required :class:`FaultScenario` objects.
 
 """
-from typing import Any
+import datetime
 import logging
 import time
-import datetime
+from typing import Any
 
 from beam_calculation.beam_calculator import BeamCalculator
-from beam_calculation.simulation_output.simulation_output import \
-    SimulationOutput
-from core.list_of_elements.factory import ListOfElementsFactory
-
-from failures.fault import Fault
-from failures import strategy
-
-from core.elements.element import Element
+from beam_calculation.simulation_output.simulation_output import (
+    SimulationOutput,
+)
 from core.accelerator.accelerator import Accelerator
-
-from optimisation.algorithms.factory import optimisation_algorithm_factory
+from core.elements.element import Element
+from core.list_of_elements.factory import ListOfElementsFactory
+from evaluator.list_of_simulation_output_evaluators import (
+    FaultScenarioSimulationOutputEvaluators,
+)
+from failures import strategy
+from failures.fault import Fault
 from optimisation.algorithms.algorithm import OptimisationAlgorithm
-from optimisation.design_space.factory import (DesignSpaceFactory,
-                                               get_design_space_factory)
-
-
+from optimisation.algorithms.factory import optimisation_algorithm_factory
+from optimisation.design_space.factory import (
+    DesignSpaceFactory,
+    get_design_space_factory,
+)
 from util import debug
-
-from evaluator.list_of_simulation_output_evaluators import \
-    FaultScenarioSimulationOutputEvaluators
 
 DISPLAY_CAVITIES_INFO = True
 
@@ -40,15 +37,17 @@ DISPLAY_CAVITIES_INFO = True
 class FaultScenario(list):
     """A class to hold all fault related data."""
 
-    def __init__(self,
-                 ref_acc: Accelerator,
-                 fix_acc: Accelerator,
-                 beam_calculator: BeamCalculator,
-                 wtf: dict[str, Any],
-                 design_space_factory: DesignSpaceFactory,
-                 fault_idx: list[int] | list[list[int]],
-                 comp_idx: list[list[int]] | None = None,
-                 info_other_sol: list[dict] | None = None) -> None:
+    def __init__(
+        self,
+        ref_acc: Accelerator,
+        fix_acc: Accelerator,
+        beam_calculator: BeamCalculator,
+        wtf: dict[str, Any],
+        design_space_factory: DesignSpaceFactory,
+        fault_idx: list[int] | list[list[int]],
+        comp_idx: list[list[int]] | None = None,
+        info_other_sol: list[dict] | None = None,
+    ) -> None:
         """Create the :class:`FaultScenario` and the :class:`.Fault` objects.
 
         .. todo::
@@ -89,14 +88,15 @@ class FaultScenario(list):
         self.info = {}
         self.optimisation_time: datetime.timedelta
 
-        indexes = strategy.sort_and_gather_faults(fix=fix_acc,
-                                                  fault_idx=fault_idx,
-                                                  comp_idx=comp_idx,
-                                                  **wtf)
-        faults = self._set_faults(indexes,
-                                  self._get_reference(),
-                                  beam_calculator.list_of_elements_factory,
-                                  design_space_factory)
+        indexes = strategy.sort_and_gather_faults(
+            fix=fix_acc, fault_idx=fault_idx, comp_idx=comp_idx, **wtf
+        )
+        faults = self._set_faults(
+            indexes,
+            self._get_reference(),
+            beam_calculator.list_of_elements_factory,
+            design_space_factory,
+        )
         super().__init__(faults)
         self._set_optimisation_algorithms()
 
@@ -108,12 +108,13 @@ class FaultScenario(list):
 
         self._break_cavities()
 
-    def _set_faults(self,
-                    indexes: tuple[list[list[int]], list[list[int]]],
-                    reference_simulation_output: SimulationOutput,
-                    list_of_elements_factory: ListOfElementsFactory,
-                    design_space_factory: DesignSpaceFactory,
-                    ) -> list[Fault]:
+    def _set_faults(
+        self,
+        indexes: tuple[list[list[int]], list[list[int]]],
+        reference_simulation_output: SimulationOutput,
+        list_of_elements_factory: ListOfElementsFactory,
+        design_space_factory: DesignSpaceFactory,
+    ) -> list[Fault]:
         """Create the :class:`.Fault` objects.
 
         Parameters
@@ -160,7 +161,8 @@ class FaultScenario(list):
         solvers_already_used = list(self.ref_acc.simulation_outputs.keys())
         assert len(solvers_already_used) > 0, (
             "You must compute propagation of the beam in the reference linac "
-            "prior to create a FaultScenario")
+            "prior to create a FaultScenario"
+        )
         solv1 = solvers_already_used[0]
         reference_simulation_output = self.ref_acc.simulation_outputs[solv1]
         return reference_simulation_output
@@ -170,21 +172,22 @@ class FaultScenario(list):
         logging.warning(
             "The phases in the broken linac are relative. It may be more "
             "relatable to use absolute phases, as it would avoid the rephasing"
-            " of the linac at each cavity.")
+            " of the linac at each cavity."
+        )
         cavities = self.fix_acc.l_cav
         first_failed_cavity = self[0].failed_elements[0]
         first_failed_index = cavities.index(first_failed_cavity)
 
         # old:
         # cavities_to_rephase = [cav for cav in cavities[first_failed_index:]
-                               # if cav.get('status') == 'nominal']
+        # if cav.get('status') == 'nominal']
         # may remove this if proven unneccessary
         for cav in cavities:
-            assert cav.status == 'nominal'
+            assert cav.status == "nominal"
         # new:
         cavities_to_rephase = cavities[first_failed_index:]
         for cav in cavities_to_rephase:
-            cav.update_status('rephased (in progress)')
+            cav.update_status("rephased (in progress)")
 
     def _set_optimisation_algorithms(self) -> list[OptimisationAlgorithm]:
         """Set each fault's optimisation algorithm.
@@ -200,22 +203,21 @@ class FaultScenario(list):
         # in the factory
         kwargs = {}
 
-        opti_method = self.wtf['optimisation_algorithm']
+        opti_method = self.wtf["optimisation_algorithm"]
         assert isinstance(opti_method, str)
 
         optimisation_algorithms = [
             optimisation_algorithm_factory(
-                opti_method,
-                fault,
-                self.beam_calculator,
-                **kwargs)
-            for fault in self]
+                opti_method, fault, self.beam_calculator, **kwargs
+            )
+            for fault in self
+        ]
         return optimisation_algorithms
 
     def _break_cavities(self) -> None:
         """Break the cavities."""
         for fault in self:
-            fault.update_elements_status(optimisation='not started')
+            fault.update_elements_status(optimisation="not started")
 
     def fix_all(self) -> None:
         """
@@ -228,47 +230,56 @@ class FaultScenario(list):
         start_time = time.monotonic()
 
         success, info = [], []
-        ref_simulation_output = \
-            self.ref_acc.simulation_outputs[self.beam_calculator.id]
+        ref_simulation_output = self.ref_acc.simulation_outputs[
+            self.beam_calculator.id
+        ]
         optimisation_algorithms = self._set_optimisation_algorithms()
 
-        for fault, optimisation_algorithm in zip(self,
-                                                 optimisation_algorithms):
+        for fault, optimisation_algorithm in zip(
+            self, optimisation_algorithms
+        ):
             _succ, optimized_cavity_settings, _info = fault.fix(
-                optimisation_algorithm)
+                optimisation_algorithm
+            )
 
             success.append(_succ)
             info.append(_info)
 
-            simulation_output = \
+            simulation_output = (
                 self.beam_calculator.post_optimisation_run_with_this(
                     optimized_cavity_settings,
                     self.fix_acc.elts,
                 )
+            )
             simulation_output.compute_complementary_data(
-                self.fix_acc.elts,
-                ref_simulation_output=ref_simulation_output)
+                self.fix_acc.elts, ref_simulation_output=ref_simulation_output
+            )
 
             self.fix_acc.keep_settings(simulation_output)
-            self.fix_acc.simulation_outputs[self.beam_calculator.id] \
-                = simulation_output
+            self.fix_acc.simulation_outputs[self.beam_calculator.id] = (
+                simulation_output
+            )
 
             fault.get_x_sol_in_real_phase(self._reference_phase)
-            fault.update_elements_status(optimisation='finished', success=True)
+            fault.update_elements_status(optimisation="finished", success=True)
 
             if not self.beam_calculator.flag_phi_abs:
                 # Tell LW to keep the new phase of the rephased cavities
                 # between the two compensation zones
                 self._reupdate_status_of_rephased_cavities(fault)
-                logging.critical("Calculation in relative phase. Check if "
-                                 "necessary to reperform simulation?")
+                logging.critical(
+                    "Calculation in relative phase. Check if "
+                    "necessary to reperform simulation?"
+                )
 
-        self.fix_acc.name = f"Fixed ({str(success.count(True))}" \
-            + f" of {str(len(success))})"
+        self.fix_acc.name = (
+            f"Fixed ({str(success.count(True))}" + f" of {str(len(success))})"
+        )
 
         for linac in [self.ref_acc, self.fix_acc]:
-            self.info[linac.name + ' cav'] = \
-                debug.output_cavities(linac, DISPLAY_CAVITIES_INFO)
+            self.info[linac.name + " cav"] = debug.output_cavities(
+                linac, DISPLAY_CAVITIES_INFO
+            )
 
         self._evaluate_fit_quality(save=True)
 
@@ -290,19 +301,21 @@ class FaultScenario(list):
         logging.warning("Changed the way of defining idx1 and idx2.")
         elts = self.fix_acc.elts
 
-        idx1 = fault.elts[-1].idx['elt_idx']
+        idx1 = fault.elts[-1].idx["elt_idx"]
         idx2 = len(elts)
         if fault is not self[-1]:
             next_fault = self[self.index(fault) + 1]
-            idx2 = next_fault.elts[0].idx['elt_idx'] + 1
+            idx2 = next_fault.elts[0].idx["elt_idx"] + 1
 
         rephased_cavities_between_two_faults = [
-            elt for elt in elts[idx1:idx2]
-            if elt.get('nature') == 'FIELD_MAP'
-            and elt.get('status') == 'rephased (in progress)']
+            elt
+            for elt in elts[idx1:idx2]
+            if elt.get("nature") == "FIELD_MAP"
+            and elt.get("status") == "rephased (in progress)"
+        ]
 
         for cav in rephased_cavities_between_two_faults:
-            cav.update_status('rephased (ok)')
+            cav.update_status("rephased (ok)")
 
     def _transfer_phi0_from_ref_to_broken(self) -> None:
         """Transfer the entry phases from reference linac to broken.
@@ -325,9 +338,12 @@ class FaultScenario(list):
         """Give the reference phase: `phi_0_rel` or 'phi_0_abs'."""
         return self.beam_calculator.reference_phase
 
-    def _evaluate_fit_quality(self, save: bool = True,
-                              id_solver_ref: str | None = None,
-                              id_solver_fix: str | None = None) -> None:
+    def _evaluate_fit_quality(
+        self,
+        save: bool = True,
+        id_solver_ref: str | None = None,
+        id_solver_fix: str | None = None,
+    ) -> None:
         """
         Compute some quantities on the whole linac to see if fit is good.
 
@@ -344,14 +360,21 @@ class FaultScenario(list):
             None. In this case, the solver is the same as for reference.
 
         """
-        simulations = self._simulations_that_should_be_compared(id_solver_ref,
-                                                                id_solver_fix)
+        simulations = self._simulations_that_should_be_compared(
+            id_solver_ref, id_solver_fix
+        )
 
-        quantities_to_evaluate = ('w_kin', 'phi_abs', 'envelope_pos_phiw',
-                                  'envelope_energy_phiw', 'eps_phiw',
-                                  'mismatch_factor_zdelta')
+        quantities_to_evaluate = (
+            "w_kin",
+            "phi_abs",
+            "envelope_pos_phiw",
+            "envelope_energy_phiw",
+            "eps_phiw",
+            "mismatch_factor_zdelta",
+        )
         my_evaluator = FaultScenarioSimulationOutputEvaluators(
-            quantities_to_evaluate, [fault for fault in self], simulations)
+            quantities_to_evaluate, [fault for fault in self], simulations
+        )
         my_evaluator.run(output=True)
 
         # if save:
@@ -359,9 +382,10 @@ class FaultScenario(list):
         #     out = os.path.join(self.fix_acc.get('beam_calc_path'), fname)
         #     df_eval.to_csv(out)
 
-    def _set_evaluation_elements(self,
-                                 additional_elt: list[Element] | None = None,
-                                 ) -> dict[str, Element]:
+    def _set_evaluation_elements(
+        self,
+        additional_elt: list[Element] | None = None,
+    ) -> dict[str, Element]:
         """Set a the proper list of where to check the fit quality."""
         evaluation_elements = [fault.elts[-1] for fault in self]
         if additional_elt is not None:
@@ -380,10 +404,12 @@ class FaultScenario(list):
             id_solver_fix = id_solver_ref
 
         if id_solver_ref != id_solver_fix:
-            logging.warning("You are trying to compare two SimulationOutputs "
-                            "created by two different solvers. This may lead "
-                            "to errors, as interpolations in this case are not"
-                            " implemented yet.")
+            logging.warning(
+                "You are trying to compare two SimulationOutputs "
+                "created by two different solvers. This may lead "
+                "to errors, as interpolations in this case are not"
+                " implemented yet."
+            )
 
         ref_simu = self.ref_acc.simulation_outputs[id_solver_ref]
         fix_simu = self.fix_acc.simulation_outputs[id_solver_fix]
@@ -418,14 +444,16 @@ def fault_scenario_factory(
         already initialied :class:`Fault` objects.
 
     """
-    scenarios_fault_idx = wtf.pop('failed')
+    scenarios_fault_idx = wtf.pop("failed")
 
     scenarios_comp_idx = [None for _ in accelerators[1:]]
-    if 'manual list' in wtf:
-        scenarios_comp_idx = wtf.pop('manual list')
+    if "manual list" in wtf:
+        scenarios_comp_idx = wtf.pop("manual list")
 
-    _ = [beam_calculator.init_solver_parameters(accelerator)
-         for accelerator in accelerators]
+    _ = [
+        beam_calculator.init_solver_parameters(accelerator)
+        for accelerator in accelerators
+    ]
 
     design_space_factory: DesignSpaceFactory
     design_space_factory = get_design_space_factory(**design_space_kw)
@@ -438,8 +466,11 @@ def fault_scenario_factory(
             wtf=wtf,
             design_space_factory=design_space_factory,
             fault_idx=fault_idx,
-            comp_idx=comp_idx)
-        for accelerator, fault_idx, comp_idx
-        in zip(accelerators[1:], scenarios_fault_idx, scenarios_comp_idx)]
+            comp_idx=comp_idx,
+        )
+        for accelerator, fault_idx, comp_idx in zip(
+            accelerators[1:], scenarios_fault_idx, scenarios_comp_idx
+        )
+    ]
 
     return fault_scenarios
