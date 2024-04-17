26943ecf4afd07b902f1a341deef2dcdcb221734
Adrien P <placais@lpsc.in2p3.fr>
2024-04-17 17:05:02 +0200

    autoformat

--- source/core/commands/helper.py
+++ source/core/commands/helper.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Define helper function applying on commands."""
-from core.instruction import Instruction
 from core.commands.command import Command
+from core.instruction import Instruction
 
 
-def apply_commands(instructions: list[Instruction],
-                   freq_bunch: float) -> list[Instruction]:
+def apply_commands(
+    instructions: list[Instruction], freq_bunch: float
+) -> list[Instruction]:
     """Apply all the implemented commands."""
     index = 0
     while index < len(instructions):
@@ -15,7 +16,8 @@ def apply_commands(instructions: list[Instruction],
         if isinstance(instruction, Command):
             instruction.set_influenced_elements(instructions)
             if instruction.is_implemented:
-                instructions = instruction.apply(instructions,
-                                                 freq_bunch=freq_bunch)
+                instructions = instruction.apply(
+                    instructions, freq_bunch=freq_bunch
+                )
         index += 1
     return instructions
