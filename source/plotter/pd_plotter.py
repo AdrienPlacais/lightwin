53d72f99e0f4b760fc757a85efb9fdabe5cbf118
Adrien P <placais@lpsc.in2p3.fr>
2024-05-06 13:40:18 +0200

    MatplotlibPlotter, that PandasPlotter will inherit from

--- source/plotter/pd_plotter.py
+++ source/plotter/pd_plotter.py
@@ -6,47 +6,22 @@
 """
 
 from collections.abc import Sequence
-from pathlib import Path
 from typing import Any
 
 import pandas as pd
 from matplotlib.axes import Axes
 
-from core.list_of_elements.list_of_elements import ListOfElements
-from plotter.i_plotter import IPlotter
-from plotter.matplotlib_helper import (
-    create_fig_if_not_exists,
-    plot_section,
-    plot_structure,
-)
+from plotter.matplotlib_plotter import MatplotlibPlotter
 from util.dicts_output import markdown
 
 
-class PandasPlotter(IPlotter):
+class PandasPlotter(MatplotlibPlotter):
     """A plotter that takes in pandas dataframe.
 
     .. note::
         Under the hood, this is matplotlib which is called.
 
     """
-
-    def __init__(self, elts: ListOfElements | None = None) -> None:
-        """Instantiate some common attributes."""
-        super().__init__(elts)
-
-    def _setup_fig(self, fignum: int, title: str, **kwargs) -> list[Axes]:
-        """Setup the figure and axes."""
-        axnum = 2
-        if not self._structure:
-            axnum = 1
-        return create_fig_if_not_exists(
-            axnum,
-            title=title,
-            sharex=self._sharex,
-            num=fignum,
-            clean_fig=True,
-            **kwargs,
-        )
 
     def _actual_plot(
         self,
@@ -68,47 +43,3 @@ class PandasPlotter(IPlotter):
             **plot_kwargs,
         )
         return axes
-
-    def save_figure(self, axes: Axes, save_path: Path) -> None:
-        figure = axes.get_figure()
-        assert figure is not None
-        return figure.savefig(save_path)
-
-    def _plot_structure(
-        self,
-        axes: Sequence[Axes],
-        elts: ListOfElements | None = None,
-        x_axis: str = "z_abs",
-    ) -> None:
-        """Add a plot to show the structure of the linac."""
-        if elts is None:
-            elts = self._elts
-            assert hasattr(self, "_elts"), (
-                "Please provide at least a defaut ListOfElements for structure"
-                " plots."
-            )
-        plot_structure(axes[-1], elts, x_axis)
-
-        if not self._sections:
-            return
-        return self._plot_sections(axes[-1], elts, x_axis)
-
-    def _plot_sections(
-        self, axes: Any, elts: ListOfElements, x_axis: str
-    ) -> None:
-        """Add the sections on the structure plot."""
-        return plot_section(axes, elts, x_axis)
-
-    def _actual_constant_plot(
-        self,
-        axes: Axes | Sequence[Axes],
-        constant: float,
-        color: str,
-        ls: str,
-        **kwargs,
-    ) -> None:
-        """Add one constant plot."""
-        if not isinstance(axes, Sequence):
-            axes = (axes,)
-        for axe in axes:
-            axe.axhline(constant, xmin=0, xmax=1, color=color, ls=ls, **kwargs)
