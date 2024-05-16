ceacd37b94716896273dfff82d967636cdf5047a
Adrien PLACAIS <placais@grenoble.in2p3.fr>
2024-05-16 10:07:38 +0200

    instructions + compatibility windows

--- source/util/setup.py
+++ source/util/setup.py
@@ -2,10 +2,17 @@
 # -*- coding: utf-8 -*-
 """Define function to build the Cython module(s).
 
-To compile, go to ``LightWin/source/`` and enter:
-``python3 util/setup.py build_ext --inplace``
-and manually move the ``build/lib.blabla/beam_calculation/envelope_1d/transfer_
-matrices_c.cpython-blabla.so`` to ``beam_calculation/envelope1d/``
+To compile, go to ``lightwin/source/`` and enter:
+``python util/setup.py build_ext --inplace``.
+
+If it is not automatic on your platform, you must manually move the created
+files to the proper location.
+
+Unix:
+    ``build/lib.linux-XXX-cpython=3XX/beam_calculation/envelope_1d/transfer_matrices_c.cpython-3XX-XXXX-linux-gnu.so``
+Windows
+    ``build/lib.win-XXXX-cpython-3XX/beam_calculation/envelope_1d/transfer_matrices_c.cp3XX-win_XXXX.pyd``
+to ``beam_calculation/envelope_1d/``
 
 .. todo::
     Auto move the ``.so``.
@@ -19,6 +26,7 @@ import os
 import os.path
 
 from Cython.Build import cythonize
+import numpy as np
 from setuptools import setup
 
 source_path = os.getcwd()
@@ -32,5 +40,6 @@ setup(
         # compiler_directives={'boundscheck': False,
         #                      'nonecheck': False,
         #                      'wraparound': False}
-    )
+    ),
+    include_dirs=[np.get_include()]
 )
