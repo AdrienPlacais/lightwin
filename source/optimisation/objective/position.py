3cd2ad05631b8b2a765ea86b122f1cb509357aad
Adrien P <placais@lpsc.in2p3.fr>
2024-05-30 10:06:57 +0200

    removed unused imports, completed todolist

--- source/optimisation/objective/position.py
+++ source/optimisation/objective/position.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """Define functions to determine the compensation zone.
 
 In most cases, we need to study only a fraction of the linac during
@@ -13,8 +11,19 @@ cavities, as well as the place where objectives are evaluated.
 .. todo::
     end_section, elt.name
 
+.. todo::
+    ``full_linac`` seems useless. Could be a strategy instead of an override
+    setting. Would need to edit the :class:`.ObjectiveFactory` too.
+
+.. note::
+    ``[*a, *b]`` will concatenate the two ``Iterable`` ``a`` and ``b``. Same as
+    ``a + b`` but will work with every ``Iterable``, not just ``list``.
+
 """
+
 import logging
+from collections.abc import Collection, Iterable
+from typing import Any
 
 from core.elements.element import Element
 from core.list_of_elements.list_of_elements import ListOfElements
@@ -22,9 +31,9 @@ from core.list_of_elements.list_of_elements import ListOfElements
 
 def zone_to_recompute(
     broken_elts: ListOfElements,
-    objective_position_preset: list[str],
-    fault_idx: list[int],
-    comp_idx: list[int],
+    objective_position_preset: Collection[str],
+    fault_idx: Iterable[int],
+    comp_idx: Iterable[int],
     full_lattices: bool = False,
     full_linac: bool = False,
     start_at_beginning_of_linac: bool = False,
@@ -37,15 +46,12 @@ def zone_to_recompute(
     ----------
     broken_elts : ListOfElements
         :class:`.ListOfElements` from the broken linac.
-    objective_position_preset : list[str]
+    objective_position_preset : Collection[str]
         Short strings that must be in ``POSITION_TO_INDEX`` dictionary to
         determine where the objectives should be evaluated.
-    fault_idx : list[int]
-        Cavity index of the faults, directly converted to element index in the
-        routine.
-    comp_idx : list[int]
-        Cavity index of the compensating cavities, directly converted to
-        element index in the routine.
+    fault_idx, comp_idx : Iterable[int]
+        Cavity index of the faults and compensating cavities, directly
+        converted to element index in the routine.
     full_lattices : bool, optional
         If you want the compensation zone to encompass full lattices only. It
         is a little bit slower as more ``Element`` are calculated. Plus,
@@ -70,11 +76,11 @@ def zone_to_recompute(
         for preset in objective_position_preset
     ]
 
-    idx_start_compensation_zone = min(fault_idx + comp_idx)
+    idx_start_compensation_zone = min([*fault_idx, *comp_idx])
 
     if start_at_beginning_of_linac:
         logging.info(
-            "Force start of compensation zone @ first element of the " "linac."
+            "Force start of compensation zone @ first element of the linac."
         )
         idx_start_compensation_zone = 0
     if full_lattices:
@@ -106,10 +112,10 @@ def _zone(preset: str, *args) -> int:
 
 
 def _end_last_altered_lattice(
-    elts: ListOfElements, fault_idx: list[int], comp_idx: list[int]
+    elts: ListOfElements, fault_idx: Iterable[int], comp_idx: Iterable[int]
 ) -> int:
     """Evaluate obj at the end of the last lattice w/ an altered cavity."""
-    idx_last = max(fault_idx + comp_idx)
+    idx_last = max([*fault_idx, *comp_idx])
     idx_lattice_last = elts[idx_last].get("lattice")
     idx_eval = elts.by_lattice[idx_lattice_last][-1].get(
         "elt_idx", to_numpy=False
@@ -118,15 +124,15 @@ def _end_last_altered_lattice(
 
 
 def _one_lattice_after_last_altered_lattice(
-    elts: ListOfElements, fault_idx: list[int], comp_idx: list[int]
+    elts: ListOfElements, fault_idx: Iterable[int], comp_idx: Iterable[int]
 ) -> int:
     """Evaluate objective one lattice after the last comp or failed cav."""
-    idx_last = max(fault_idx + comp_idx)
+    idx_last = max([*fault_idx, *comp_idx])
     idx_lattice_last = elts[idx_last].get("lattice") + 1
     if idx_lattice_last > len(elts.by_lattice):
         logging.warning(
-            "You asked for a lattice after the end of the linac. "
-            "Revert back to previous lattice, i.e. end of linac."
+            "You asked for a lattice after the end of the linac. Revert back "
+            "to previous lattice, i.e. end of linac."
         )
         idx_lattice_last -= 1
     idx_eval = elts.by_lattice[idx_lattice_last][-1].get(
@@ -136,7 +142,7 @@ def _one_lattice_after_last_altered_lattice(
 
 
 def _end_last_failed_lattice(
-    elts: ListOfElements, fault_idx: list[int], comp_idx: list[int]
+    elts: ListOfElements, fault_idx: Collection[int], comp_idx: Collection[int]
 ) -> int:
     """Evaluate obj at the end of the last lattice w/ a failed cavity."""
     idx_last = max(fault_idx)
@@ -148,7 +154,7 @@ def _end_last_failed_lattice(
 
 
 def _one_lattice_after_last_failed_lattice(
-    elts: ListOfElements, fault_idx: list[int], comp_idx: list[int]
+    elts: ListOfElements, fault_idx: Collection[int], comp_idx: Collection[int]
 ) -> int:
     """Evaluate 1 lattice after end of the last lattice w/ a failed cavity."""
     idx_after = max(fault_idx) + 1
@@ -159,16 +165,13 @@ def _one_lattice_after_last_failed_lattice(
     return idx_eval
 
 
-def _end_linac(
-    elts: ListOfElements, fault_idx: list[int], comp_idx: list[int]
-) -> int:
+def _end_linac(elts: ListOfElements, fault_idx: Any, comp_idx: Any) -> int:
     """Evaluate objective at the end of the linac."""
     return elts[-1].get("elt_idx")
 
 
 def _reduce_idx_start_to_include_full_lattice(
-    idx: int,
-    elts: ListOfElements,
+    idx: int, elts: ListOfElements
 ) -> int:
     """Force compensation zone to start at the 1st element of lattice."""
     elt = elts[idx]
