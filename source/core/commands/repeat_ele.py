c8f033065b5a863120f26acb37fc2bced76a02f2
Adrien P <placais@lpsc.in2p3.fr>
2024-06-05 20:45:42 +0200

    first basic implementation for REPEAT_ELE

--- source/core/commands/repeat_ele.py
+++ source/core/commands/repeat_ele.py
@@ -1,18 +1,69 @@
-"""Define a useless command; raise warning as influences the linac design."""
+"""Define the REPEAT_ELE command."""
 
 import logging
+from collections.abc import Sequence
+from copy import deepcopy
 
-from core.commands.dummy_command import DummyCommand
+from core.commands.command import Command
+from core.commands.lattice import Lattice, LatticeEnd
+from core.commands.set_adv import SetAdv
+from core.instruction import Comment, Instruction
 
 
-class RepeatEle(DummyCommand):
-    """A class that does nothing but raise an error."""
+class RepeatEle(Command):
+    """Repeat the ``n`` following elements ``k`` times."""
 
-    def __init__(self, *args, **kwargs) -> None:
-        """Raise an error."""
-        logging.error(
-            "The REPEAT_ELE is not implemented in LightWin. As this command "
-            "will influence the design of the linac, you should set the design"
-            " and comment this command out."
+    is_implemented = True
+    n_attributes = 2
+
+    def __init__(self, line: list[str], dat_idx: int, **kwargs: str) -> None:
+        """Instantiate object."""
+        logging.warning(
+            "REPEAT_ELE under implementation. Behavior not tested w.r.t "
+            "lattice number."
         )
-        return super().__init__(*args, **kwargs)
+        super().__init__(line, dat_idx)
+        self.k_times = int(line[1])
+        self.n_following = int(line[2])
+
+    def set_influenced_elements(
+        self, instructions: Sequence[Instruction], **kwargs: float
+    ) -> None:
+        """Capture ``n`` following elements, as well as their commands."""
+        start = self.idx["dat_idx"] + 1
+
+        number_of_elements = 0
+        stop = start
+        for instruction in instructions[start:]:
+
+            if number_of_elements == self.n_following:
+                self.influenced = slice(start, stop)
+                return
+
+            stop += 1
+            if isinstance(instruction, RepeatEle):
+                raise IOError("I think nested REPEAT_ELE are not allowed.")
+
+            if isinstance(instruction, (Lattice, LatticeEnd, SetAdv)):
+                logging.error(
+                    "According to doc, LATTICE, LATTICE_END and SET_ADV "
+                    "commands should not be duplicated. Still unsure about how"
+                    " I will treat that."
+                )
+                continue
+
+            if isinstance(instruction, (Command, Comment)):
+                continue
+
+            number_of_elements += 1
+        raise IOError("Reached end of file without completing REPEAT_ELE.")
+
+    def apply(
+        self, instructions: list[Instruction], **kwargs: float
+    ) -> list[Instruction]:
+        """Set lattice section number of elements in current lattice."""
+        repeated_instructions = instructions[self.influenced]
+        for _ in range(self.k_times):
+            for instruction in repeated_instructions:
+                deepcopy(instruction).insert_object(instructions)
+        return instructions
